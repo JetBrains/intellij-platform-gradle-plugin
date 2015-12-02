5a83e8fa21254637483a98da202685ca3c6b706f
Alexander Zolotov <goldifit@gmail.com>
2015-12-02 19:27:12 +0300

    Disable notnull instrumenting on missing ClassFilterAnnotationRegexp class
    Fixes #28

--- src/main/groovy/org/jetbrains/intellij/IntelliJInstrumentCodeAction.groovy
+++ src/main/groovy/org/jetbrains/intellij/IntelliJInstrumentCodeAction.groovy
@@ -1,15 +1,19 @@
 package org.jetbrains.intellij
 
+import org.apache.tools.ant.BuildException
 import org.gradle.api.Action
 import org.gradle.api.Task
+import org.gradle.api.file.ConfigurableFileCollection
 import org.gradle.api.tasks.compile.AbstractCompile
 import org.jetbrains.annotations.NotNull
 
 class IntelliJInstrumentCodeAction implements Action<Task> {
+    private static final String FILTER_ANNOTATION_REGEXP_CLASS = 'com.intellij.ant.ClassFilterAnnotationRegexp'
+    private static final LOADER_REF = "java2.loader"
+
     @Override
     void execute(Task task) {
         def extension = task.project.extensions.getByType(IntelliJPluginExtension)
-        def loader = "java2.loader"
         def classpath = task.project.files(
                 "$extension.ideaDirectory/lib/javac2.jar",
                 "$extension.ideaDirectory/lib/jdom.jar",
@@ -17,27 +21,49 @@ class IntelliJInstrumentCodeAction implements Action<Task> {
                 "$extension.ideaDirectory/lib/jgoodies-forms.jar")
         task.project.ant.taskdef(name: 'instrumentIdeaExtensions',
                 classpath: classpath.asPath,
-                loaderref: loader,
+                loaderref: LOADER_REF,
                 classname: 'com.intellij.ant.InstrumentIdeaExtensions')
-        task.project.ant.typedef(name: 'skip',
-                classpath: classpath.asPath,
-                loaderref: loader,
-                classname: 'com.intellij.ant.ClassFilterAnnotationRegexp')
 
-        assert task instanceof AbstractCompile
         IntelliJPlugin.LOG.info("Compiling forms and instrumenting code with nullability preconditions")
+        boolean instrumentNotNull = prepareNotNullInstrumenting(task, classpath)
+        assert task instanceof AbstractCompile
         def srcDirs = Utils.mainSourceSet(task.project).compiledBy(task).java.srcDirs.findAll { it.exists() } +
                 Utils.testSourceSet(task.project).compiledBy(task).java.srcDirs.findAll { it.exists() };
         if (!srcDirs.empty) {
-            instrumentCode(task, srcDirs)
+            instrumentCode(task, srcDirs, instrumentNotNull)
         }
     }
 
-    private static void instrumentCode(@NotNull AbstractCompile compileTask, @NotNull Collection<File> srcDirs) {
+    private static boolean prepareNotNullInstrumenting(@NotNull Task task,
+                                                       @NotNull ConfigurableFileCollection classpath) {
+        try {
+            task.project.ant.typedef(name: 'skip',
+                    classpath: classpath.asPath,
+                    loaderref: LOADER_REF,
+                    classname: FILTER_ANNOTATION_REGEXP_CLASS)
+        } catch (BuildException e) {
+            def cause = e.getCause()
+            if (cause instanceof ClassNotFoundException && FILTER_ANNOTATION_REGEXP_CLASS.equals(cause.getMessage())) {
+                IntelliJPlugin.LOG.info("Old version of Javac2 is used, " +
+                        "instrumenting code with nullability will be skipped. Use IDEA >14 SDK (139.*) to fix this")
+                return false
+            } else {
+                throw e
+            }
+        }
+        return true
+    }
+
+    private static void instrumentCode(@NotNull AbstractCompile compileTask,
+                                       @NotNull Collection<File> srcDirs,
+                                       boolean instrumentNotNull) {
         def headlessOldValue = System.setProperty('java.awt.headless', 'true')
         compileTask.project.ant.instrumentIdeaExtensions(srcdir: compileTask.project.files(srcDirs).asPath,
-                destdir: compileTask.destinationDir, classpath: compileTask.classpath.asPath) {
-            compileTask.project.ant.skip(pattern: 'kotlin/jvm/internal/.*')
+                destdir: compileTask.destinationDir, classpath: compileTask.classpath.asPath,
+                instrumentNotNull: instrumentNotNull) {
+            if (instrumentNotNull) {
+                compileTask.project.ant.skip(pattern: 'kotlin/jvm/internal/.*')
+            }
         }
         if (headlessOldValue != null) {
             System.setProperty('java.awt.headless', headlessOldValue)
@@ -45,4 +71,5 @@ class IntelliJInstrumentCodeAction implements Action<Task> {
             System.clearProperty('java.awt.headless')
         }
     }
+
 }
