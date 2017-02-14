066c454ea74c709c9c53969b96121181f3fec973
Minecrell <dev@minecrell.net>
2017-02-14 17:19:13 +0100

    Move code instrumentation to extra task

--- src/main/groovy/org/jetbrains/intellij/tasks/IntelliJInstrumentCodeTask.groovy
+++ src/main/groovy/org/jetbrains/intellij/tasks/IntelliJInstrumentCodeTask.groovy
@@ -1,67 +1,82 @@
 package org.jetbrains.intellij.tasks
 
 import org.apache.tools.ant.BuildException
-import org.gradle.api.Action
-import org.gradle.api.Task
+import org.gradle.api.DefaultTask
 import org.gradle.api.file.ConfigurableFileCollection
+import org.gradle.api.file.FileCollection
+import org.gradle.api.file.FileTree
 import org.gradle.api.file.SourceDirectorySet
-import org.gradle.api.tasks.compile.AbstractCompile
+import org.gradle.api.tasks.*
 import org.jetbrains.annotations.NotNull
-import org.jetbrains.intellij.IntelliJPlugin
-import org.jetbrains.intellij.IntelliJPluginExtension
-import org.jetbrains.intellij.Utils
+import org.jetbrains.intellij.dependency.IdeaDependency
 
-class IntelliJInstrumentCodeAction implements Action<AbstractCompile> {
+class IntelliJInstrumentCodeTask extends DefaultTask {
     private static final String FILTER_ANNOTATION_REGEXP_CLASS = 'com.intellij.ant.ClassFilterAnnotationRegexp'
     private static final LOADER_REF = "java2.loader"
-    private final boolean myTestInstrumentation
 
-    IntelliJInstrumentCodeAction(boolean testInstrumentation) {
-        myTestInstrumentation = testInstrumentation
+    SourceSet sourceSet
+
+    @Input
+    IdeaDependency ideaDependency
+
+    @OutputDirectory
+    File output
+
+    @InputFiles
+    @SkipWhenEmpty
+    FileTree getOriginalClasses() {
+        return project.fileTree(sourceSet.output.classesDir)
     }
 
-    @Override
-    void execute(AbstractCompile task) {
-        def extension = task.project.extensions.getByType(IntelliJPluginExtension)
-        if (extension == null || !extension.instrumentCode || extension.type == 'RS') {
-            return
-        }
-        def classpath = task.project.files(
-                "$extension.ideaDependency.classes/lib/javac2.jar",
-                "$extension.ideaDependency.classes/lib/jdom.jar",
-                "$extension.ideaDependency.classes/lib/asm-all.jar",
-                "$extension.ideaDependency.classes/lib/jgoodies-forms.jar")
-        task.project.ant.taskdef(name: 'instrumentIdeaExtensions',
+    @InputFiles
+    FileCollection getSourceDirs() {
+        def sourceDirs = existingDirs(sourceSet.allSource)
+        sourceDirs.removeAll(sourceSet.resources)
+        return project.files(sourceDirs)
+    }
+
+    @TaskAction
+    void instrumentClasses() {
+        copyOriginalClasses()
+
+        def classpath = project.files(
+                "$ideaDependency.classes/lib/javac2.jar",
+                "$ideaDependency.classes/lib/jdom.jar",
+                "$ideaDependency.classes/lib/asm-all.jar",
+                "$ideaDependency.classes/lib/jgoodies-forms.jar")
+
+        project.ant.taskdef(name: 'instrumentIdeaExtensions',
                 classpath: classpath.asPath,
                 loaderref: LOADER_REF,
                 classname: 'com.intellij.ant.InstrumentIdeaExtensions')
 
-        IntelliJPlugin.LOG.info("Compiling forms and instrumenting code with nullability preconditions")
-        //noinspection GroovyAssignabilityCheck
-        boolean instrumentNotNull = prepareNotNullInstrumenting(task, classpath)
-        def sourceSet = myTestInstrumentation ?
-                Utils.testSourceSet(task.project).compiledBy(task) :
-                Utils.mainSourceSet(task.project).compiledBy(task)
-        def srcDirs = existingDirs(sourceSet.allSource)
-        srcDirs.removeAll(existingDirs(sourceSet.resources))
-        if (!srcDirs.empty) {
-            instrumentCode(task, srcDirs, instrumentNotNull)
-        }
+        logger.info("Compiling forms and instrumenting code with nullability preconditions")
+        boolean instrumentNotNull = prepareNotNullInstrumenting(classpath)
+        instrumentCode(sourceDirs, instrumentNotNull)
     }
 
-    private static HashSet<File> existingDirs(SourceDirectorySet sourceDirectorySet) {
+    private static HashSet<File> existingDirs(@NotNull SourceDirectorySet sourceDirectorySet) {
         return sourceDirectorySet.srcDirs.findAll { it.exists() }
     }
 
-    private static boolean prepareNotNullInstrumenting(@NotNull Task compileTask,
-                                                       @NotNull ConfigurableFileCollection classpath) {
+    private void copyOriginalClasses() {
+        output.deleteDir()
+
+        // Copy original classes
+        project.copy {
+            from originalClasses
+            into output
+        }
+    }
+
+    private boolean prepareNotNullInstrumenting(@NotNull ConfigurableFileCollection classpath) {
         try {
-            compileTask.project.ant.typedef(name: 'skip', classpath: classpath.asPath, loaderref: LOADER_REF,
+            project.ant.typedef(name: 'skip', classpath: classpath.asPath, loaderref: LOADER_REF,
                     classname: FILTER_ANNOTATION_REGEXP_CLASS)
         } catch (BuildException e) {
             def cause = e.getCause()
             if (cause instanceof ClassNotFoundException && FILTER_ANNOTATION_REGEXP_CLASS.equals(cause.getMessage())) {
-                IntelliJPlugin.LOG.info("Old version of Javac2 is used, " +
+                logger.info("Old version of Javac2 is used, " +
                         "instrumenting code with nullability will be skipped. Use IDEA >14 SDK (139.*) to fix this")
                 return false
             } else {
@@ -71,15 +86,13 @@ class IntelliJInstrumentCodeAction implements Action<AbstractCompile> {
         return true
     }
 
-    private static void instrumentCode(@NotNull AbstractCompile compileTask,
-                                       @NotNull Collection<File> srcDirs,
-                                       boolean instrumentNotNull) {
+    private void instrumentCode(@NotNull FileCollection srcDirs, boolean instrumentNotNull) {
         def headlessOldValue = System.setProperty('java.awt.headless', 'true')
-        compileTask.project.ant.instrumentIdeaExtensions(srcdir: compileTask.project.files(srcDirs).asPath,
-                destdir: compileTask.destinationDir, classpath: compileTask.classpath.asPath,
+        project.ant.instrumentIdeaExtensions(srcdir: srcDirs.asPath,
+                destdir: output, classpath: sourceSet.compileClasspath.asPath,
                 includeantruntime: false, instrumentNotNull: instrumentNotNull) {
             if (instrumentNotNull) {
-                compileTask.project.ant.skip(pattern: 'kotlin/Metadata')
+                project.ant.skip(pattern: 'kotlin/Metadata')
             }
         }
         if (headlessOldValue != null) {
