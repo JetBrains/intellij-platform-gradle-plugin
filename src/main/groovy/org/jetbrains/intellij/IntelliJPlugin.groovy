f595b67fafdcf8bc1cc84c082ef3ddc8ecb8f51f
Alexander Zolotov <goldifit@gmail.com>
2017-03-27 22:07:46 +0300

    compile/compile_only configuraiton should extend both `idea` and `ideaPlugins` configurations

--- src/main/groovy/org/jetbrains/intellij/IntelliJPlugin.groovy
+++ src/main/groovy/org/jetbrains/intellij/IntelliJPlugin.groovy
@@ -87,10 +87,10 @@ class IntelliJPlugin implements Plugin<Project> {
         Configuration ideaPlugins = project.configurations.create('ideaPlugins')
 
         if (JavaPlugin.hasProperty('COMPILE_ONLY_CONFIGURATION_NAME')) {
-            project.configurations.getByName(JavaPlugin.COMPILE_ONLY_CONFIGURATION_NAME).extendsFrom ideaPlugins
-            project.configurations.getByName(JavaPlugin.TEST_COMPILE_ONLY_CONFIGURATION_NAME).extendsFrom ideaPlugins
+            project.configurations.getByName(JavaPlugin.COMPILE_ONLY_CONFIGURATION_NAME).extendsFrom idea, ideaPlugins
+            project.configurations.getByName(JavaPlugin.TEST_COMPILE_ONLY_CONFIGURATION_NAME).extendsFrom idea, ideaPlugins
         } else {
-            project.configurations.getByName(JavaPlugin.COMPILE_CONFIGURATION_NAME).extendsFrom ideaPlugins
+            project.configurations.getByName(JavaPlugin.COMPILE_CONFIGURATION_NAME).extendsFrom idea, ideaPlugins
         }
 
         configureIntellijDependency(project, extension, idea)
