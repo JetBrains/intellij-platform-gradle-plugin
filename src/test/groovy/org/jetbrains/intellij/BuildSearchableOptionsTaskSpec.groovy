719928c502468df997e5a65f53d4c6b6bbba0ba4
Artem Khvastunov <artem.khvastunov@jetbrains.com>
2019-04-08 20:43:22 +0200

    #352 add tests for buildSearchableOptions and jarSearchableOptions

--- src/test/groovy/org/jetbrains/intellij/BuildSearchableOptionsTaskSpec.groovy
+++ src/test/groovy/org/jetbrains/intellij/BuildSearchableOptionsTaskSpec.groovy
@@ -1,14 +1,32 @@
 package org.jetbrains.intellij
 
-class BuildSearchableOptionsTaskSpec extends IntelliJPluginSpecBase {
+class BuildSearchableOptionsTaskSpec extends SearchableOptionsSpecBase {
     def 'skip building searchable options using IDEA prior 2019.1'() {
         given:
-        buildFile << 'intellij { }'
+        buildFile << "intellij { version = '2017.2.5' }"
 
         when:
         def result = build(IntelliJPlugin.BUILD_SEARCHABLE_OPTIONS_TASK_NAME)
 
         then:
         result.output.contains("$IntelliJPlugin.BUILD_SEARCHABLE_OPTIONS_TASK_NAME SKIPPED")
+    }
+
+    def 'build searchable options produces XML'() {
+        given:
+        pluginXml << pluginXmlWithSearchableConfigurable
+        buildFile << "intellij { version = '$intellijVersion' }"
+        testSearchableConfigurableJava << searchableConfigurableCode
+
+        when:
+        def result = build(IntelliJPlugin.BUILD_SEARCHABLE_OPTIONS_TASK_NAME)
+
+        then:
+        result.output.contains("Starting searchable options index builder")
+        result.output.contains("Searchable options index builder completed")
+
+        def text = getSearchableOptionsXml("projectName").getText("UTF-8")
+        text.contains("<configurable id=\"test.searchable.configurable\" configurable_name=\"Test Searchable Configurable\">")
+        text.contains("hit=\"Label for Test Searchable Configurable\"")
     }
 }
--- src/test/groovy/org/jetbrains/intellij/JarSearchableOptionsTaskSpec.groovy
+++ src/test/groovy/org/jetbrains/intellij/JarSearchableOptionsTaskSpec.groovy
@@ -1,14 +1,29 @@
 package org.jetbrains.intellij
 
-class JarSearchableOptionsTaskSpec extends IntelliJPluginSpecBase {
+class JarSearchableOptionsTaskSpec extends SearchableOptionsSpecBase {
     def 'skip jarring searchable options using IDEA prior 2019.1'() {
         given:
-        buildFile << 'intellij { }'
+        buildFile << "intellij { version = '2017.2.5' }"
 
         when:
         def result = build(IntelliJPlugin.JAR_SEARCHABLE_OPTIONS_TASK_NAME)
 
         then:
         result.output.contains("$IntelliJPlugin.JAR_SEARCHABLE_OPTIONS_TASK_NAME SKIPPED")
+    }
+
+    def 'jar searchable options produces archive'() {
+        given:
+        pluginXml << pluginXmlWithSearchableConfigurable
+        buildFile << "intellij { version = '$intellijVersion' }"
+        testSearchableConfigurableJava << searchableConfigurableCode
+
+        when:
+        build(IntelliJPlugin.JAR_SEARCHABLE_OPTIONS_TASK_NAME)
+
+        then:
+        def libsSearchableOptions = new File(buildDirectory, 'libsSearchableOptions')
+        libsSearchableOptions.exists()
+        collectPaths(libsSearchableOptions) == ['/lib/searchableOptions.jar'] as Set
     }
 }
