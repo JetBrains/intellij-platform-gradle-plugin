7ace36042ebc8f9812527ae774b88d7c80120af7
Alexander Zolotov <goldifit@gmail.com>
2019-11-06 13:04:07 +0300

    Fix tests

--- src/test/groovy/org/jetbrains/intellij/PublishTaskSpec.groovy
+++ src/test/groovy/org/jetbrains/intellij/PublishTaskSpec.groovy
@@ -12,7 +12,7 @@ class PublishTaskSpec extends IntelliJPluginSpecBase {
 
     def 'skip publishing plugin is distribution file is missing'() {
         given:
-        buildFile << "publishPlugin { username = 'username'; password = 'password'; distributionFile = null; }\n" +
+        buildFile << "publishPlugin { token = 'asd'; distributionFile = null; }\n" +
                 "verifyPlugin { ignoreFailures = true }"
 
         when:
@@ -20,28 +20,6 @@ class PublishTaskSpec extends IntelliJPluginSpecBase {
 
         then:
         result.output.contains('No value has been specified for property \'distributionFile\'')
-    }
-
-    def 'skip publishing if username is missing'() {
-        given:
-        buildFile << "publishPlugin { password = 'pass' }\nverifyPlugin { ignoreFailures = true }"
-
-        when:
-        def result = buildAndFail(IntelliJPlugin.PUBLISH_PLUGIN_TASK_NAME)
-
-        then:
-        result.output.contains('No value has been specified for property \'username\'')
-    }
-
-    def 'skip publishing if password is missing'() {
-        given:
-        buildFile << "publishPlugin { username = 'username' }\nverifyPlugin { ignoreFailures = true }"
-
-        when:
-        def result = buildAndFail(IntelliJPlugin.PUBLISH_PLUGIN_TASK_NAME)
-
-        then:
-        result.output.contains('No value has been specified for property \'password\'')
     }
 
     def 'skip publishing if token is missing'() {
