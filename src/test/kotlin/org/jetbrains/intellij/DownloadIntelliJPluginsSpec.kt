72ecd0134987984ac09853ceb1c3987757215030
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2021-02-10 11:47:37 +0100

    Kotlin: DownloadIntelliJPluginsSpec

--- src/test/kotlin/org/jetbrains/intellij/DownloadIntelliJPluginsSpec.kt
+++ src/test/kotlin/org/jetbrains/intellij/DownloadIntelliJPluginsSpec.kt
@@ -1,81 +1,127 @@
 package org.jetbrains.intellij
 
 import org.gradle.api.plugins.BasePlugin
+import java.io.File
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertTrue
 
-class DownloadIntelliJPluginsSpec extends IntelliJPluginSpecBase {
-    def mavenCacheDir = new File(gradleHome, 'caches/modules-2/files-2.1/')
-    def pluginsCacheDir = new File(gradleHome, 'caches/modules-2/files-2.1/com.jetbrains.intellij.idea/')
+class DownloadIntelliJPluginsSpec : IntelliJPluginSpecBase() {
 
-    @Override
-    def setup() {
-        mavenCacheDir.deleteDir()
-        pluginsCacheDir.deleteDir()
+    private val mavenCacheDir = File(gradleHome, "caches/modules-2/files-2.1/")
+    private val pluginsCacheDir = File(gradleHome, "caches/modules-2/files-2.1/com.jetbrains.intellij.idea/")
+
+    @BeforeTest
+    override fun setUp() {
+        super.setUp()
+
+        mavenCacheDir.delete()
+        pluginsCacheDir.delete()
     }
 
-    def 'download zip plugin from non-default channel'() {
-        given:
-        buildFile << 'intellij { plugins = ["CSS-X-Fire:1.55@nightly"] }'
+    @Test
+    fun `download zip plugin from non-default channel`() {
+        buildFile.groovy("""
+            intellij {
+                plugins = ["CSS-X-Fire:1.55@nightly"]
+            }
+        """)
 
-        when:
         build(BasePlugin.ASSEMBLE_TASK_NAME)
 
-        then:
-        def pluginDir = new File(mavenCacheDir, 'nightly.com.jetbrains.plugins/CSS-X-Fire/1.55')
-        pluginDir.list().contains('b36713d18b7845349268d9ba4ce4fedaff50d7a5')
-        new File(pluginDir, 'b36713d18b7845349268d9ba4ce4fedaff50d7a5').list().contains('CSS-X-Fire-1.55.zip')
-        new File(pluginsCacheDir, 'unzipped.nightly.com.jetbrains.plugins').list().contains('CSS-X-Fire-1.55')
+        val pluginDir = File(mavenCacheDir, "nightly.com.jetbrains.plugins/CSS-X-Fire/1.55")
+        pluginDir.list()?.let {
+            assertTrue(it.contains("b36713d18b7845349268d9ba4ce4fedaff50d7a5"))
+        }
+
+        File(pluginDir, "b36713d18b7845349268d9ba4ce4fedaff50d7a5").list()?.let {
+            assertTrue(it.contains("CSS-X-Fire-1.55.zip"))
+        }
+        File(pluginsCacheDir, "unzipped.nightly.com.jetbrains.plugins").list()?.let {
+            assertTrue(it.contains("CSS-X-Fire-1.55"))
+        }
     }
 
-    def 'download zip plugin'() {
-        given:
-        buildFile << 'intellij { plugins = ["org.intellij.plugins.markdown:201.6668.74"] }'
+    @Test
+    fun `download zip plugin`() {
+        buildFile.groovy("""
+            intellij {
+                plugins = ["org.intellij.plugins.markdown:201.6668.74"]
+            }
+        """)
 
-        when:
         build(BasePlugin.ASSEMBLE_TASK_NAME)
 
-        then:
-        def pluginDir = new File(mavenCacheDir, 'com.jetbrains.plugins/org.intellij.plugins.markdown/201.6668.74')
-        pluginDir.list().contains('5c9f1865c461d37e60c7083e8db9ad40c4bed98c')
-        new File(pluginDir, '5c9f1865c461d37e60c7083e8db9ad40c4bed98c').list().contains('org.intellij.plugins.markdown-201.6668.74.zip')
-        new File(pluginsCacheDir, 'unzipped.com.jetbrains.plugins').list().contains('org.intellij.plugins.markdown-201.6668.74')
+        val pluginDir = File(mavenCacheDir, "com.jetbrains.plugins/org.intellij.plugins.markdown/201.6668.74")
+        pluginDir.list()?.let {
+            assertTrue(it.contains("5c9f1865c461d37e60c7083e8db9ad40c4bed98c"))
+        }
+
+        File(pluginDir, "5c9f1865c461d37e60c7083e8db9ad40c4bed98c").list()?.let {
+            assertTrue(it.contains("org.intellij.plugins.markdown-201.6668.74.zip"))
+        }
+        File(pluginsCacheDir, "unzipped.com.jetbrains.plugins").list()?.let {
+            assertTrue(it.contains("org.intellij.plugins.markdown-201.6668.74"))
+        }
     }
 
-    def 'download jar plugin'() {
-        given:
-        buildFile << 'intellij { plugins = ["org.jetbrains.postfixCompletion:0.8-beta"] }'
+    @Test
+    fun `download jar plugin`() {
+        buildFile.groovy("""
+            intellij {
+                plugins = ["org.jetbrains.postfixCompletion:0.8-beta"]
+            }
+        """)
 
-        when:
         build(BasePlugin.ASSEMBLE_TASK_NAME)
 
-        then:
-        def pluginDir = new File(mavenCacheDir, 'com.jetbrains.plugins/org.jetbrains.postfixCompletion/0.8-beta')
-        pluginDir.list().contains('4c0b55a3dea095dd8e085b1c65fcc7b917b74dd5')
-        new File(pluginDir, '4c0b55a3dea095dd8e085b1c65fcc7b917b74dd5').list().contains('org.jetbrains.postfixCompletion-0.8-beta.jar')
+        val pluginDir = File(mavenCacheDir, "com.jetbrains.plugins/org.jetbrains.postfixCompletion/0.8-beta")
+        pluginDir.list()?.let {
+            assertTrue(it.contains("4c0b55a3dea095dd8e085b1c65fcc7b917b74dd5"))
+        }
+        File(pluginDir, "4c0b55a3dea095dd8e085b1c65fcc7b917b74dd5").list()?.let {
+            assertTrue(it.contains("org.jetbrains.postfixCompletion-0.8-beta.jar"))
+        }
     }
 
-    def 'download plugin from custom repository'() {
-        URL resource = this.getClass().getClassLoader().getResource("custom-repo/updatePlugins.xml")
+    @Test
+    fun `download plugin from custom repository`() {
+        val resource = javaClass.classLoader.getResource("custom-repo/updatePlugins.xml")
 
-        given:
-        buildFile << "intellij { pluginsRepo { custom('${resource}') }; plugins = [\"com.intellij.plugins.emacskeymap:201.6251.22\"] }"
+        buildFile.groovy("""
+            intellij {
+                pluginsRepo {
+                    custom('${resource}')
+                }
+                plugins = ["com.intellij.plugins.emacskeymap:201.6251.22"]
+            }
+        """)
 
-        when:
         build(BasePlugin.ASSEMBLE_TASK_NAME)
 
-        then:
-        new File(pluginsCacheDir, 'unzipped.com.jetbrains.plugins').list().contains('com.intellij.plugins.emacskeymap-201.6251.22')
+        File(pluginsCacheDir, "unzipped.com.jetbrains.plugins").list()?.let {
+            assertTrue(it.contains("com.intellij.plugins.emacskeymap-201.6251.22"))
+        }
     }
 
-    def 'download plugin from custom repository 2'() {
-        URL resource = this.getClass().getClassLoader().getResource("custom-repo/plugins.xml")
+    @Test
+    fun `download plugin from custom repository 2`() {
+        val resource = javaClass.classLoader.getResource("custom-repo/plugins.xml")
 
-        given:
-        buildFile << "intellij { pluginsRepo { custom('${resource}') }; plugins = [\"com.intellij.plugins.emacskeymap:201.6251.22\"] }"
+        buildFile.groovy("""
+            intellij {
+                pluginsRepo {
+                    custom('${resource}')
+                }
+                
+                plugins = ["com.intellij.plugins.emacskeymap:201.6251.22"]
+            } 
+        """)
 
-        when:
         build(BasePlugin.ASSEMBLE_TASK_NAME)
 
-        then:
-        new File(pluginsCacheDir, 'unzipped.com.jetbrains.plugins').list().contains('com.intellij.plugins.emacskeymap-201.6251.22')
+        File(pluginsCacheDir, "unzipped.com.jetbrains.plugins").list()?.let {
+            assertTrue(it.contains("com.intellij.plugins.emacskeymap-201.6251.22"))
+        }
     }
 }
