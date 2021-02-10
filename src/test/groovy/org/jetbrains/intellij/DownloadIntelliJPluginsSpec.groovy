6afd57641e221628712fd1ca507aa4ec7708d62b
Alexander Zolotov <zolotov@jetbrains.com>
2021-02-10 18:27:49 +0100

    Support queries in custom repository urls
    Fixes #589

--- src/test/groovy/org/jetbrains/intellij/DownloadIntelliJPluginsSpec.groovy
+++ src/test/groovy/org/jetbrains/intellij/DownloadIntelliJPluginsSpec.groovy
@@ -3,13 +3,17 @@ package org.jetbrains.intellij
 import org.gradle.api.plugins.BasePlugin
 
 class DownloadIntelliJPluginsSpec extends IntelliJPluginSpecBase {
-    def mavenCacheDir = new File(gradleHome, 'caches/modules-2/files-2.1/')
-    def pluginsCacheDir = new File(gradleHome, 'caches/modules-2/files-2.1/com.jetbrains.intellij.idea/')
+    def pluginsRepoCacheDir = new File(gradleHome, 'caches/modules-2/files-2.1/com.jetbrains.plugins')
+    def pluginsNightlyRepoCacheDir = new File(gradleHome, 'caches/modules-2/files-2.1/nightly.com.jetbrains.plugins')
+    def pluginsCacheDir = new File(gradleHome, 'caches/modules-2/files-2.1/com.jetbrains.intellij.idea/unzipped.com.jetbrains.plugins')
+    def pluginsNightlyCacheDir = new File(gradleHome, 'caches/modules-2/files-2.1/com.jetbrains.intellij.idea/unzipped.nightly.com.jetbrains.plugins')
 
     @Override
     def setup() {
-        mavenCacheDir.deleteDir()
+        pluginsRepoCacheDir.deleteDir()
+        pluginsNightlyRepoCacheDir.deleteDir()
         pluginsCacheDir.deleteDir()
+        pluginsNightlyCacheDir.deleteDir()
     }
 
     def 'download zip plugin from non-default channel'() {
@@ -20,10 +24,10 @@ class DownloadIntelliJPluginsSpec extends IntelliJPluginSpecBase {
         build(BasePlugin.ASSEMBLE_TASK_NAME)
 
         then:
-        def pluginDir = new File(mavenCacheDir, 'nightly.com.jetbrains.plugins/CSS-X-Fire/1.55')
-        pluginDir.list().contains('b36713d18b7845349268d9ba4ce4fedaff50d7a5')
-        new File(pluginDir, 'b36713d18b7845349268d9ba4ce4fedaff50d7a5').list().contains('CSS-X-Fire-1.55.zip')
-        new File(pluginsCacheDir, 'unzipped.nightly.com.jetbrains.plugins').list().contains('CSS-X-Fire-1.55')
+        def pluginDir = new File(pluginsNightlyRepoCacheDir, 'CSS-X-Fire/1.55')
+        pluginDir.list().contains('9cab70cc371b245cd808ade65630f505a6443b0d')
+        new File(pluginDir, '9cab70cc371b245cd808ade65630f505a6443b0d').list().contains('CSS-X-Fire-1.55.zip')
+        pluginsNightlyCacheDir.list().contains('CSS-X-Fire-1.55')
     }
 
     def 'download zip plugin'() {
@@ -34,10 +38,10 @@ class DownloadIntelliJPluginsSpec extends IntelliJPluginSpecBase {
         build(BasePlugin.ASSEMBLE_TASK_NAME)
 
         then:
-        def pluginDir = new File(mavenCacheDir, 'com.jetbrains.plugins/org.intellij.plugins.markdown/201.6668.74')
-        pluginDir.list().contains('5c9f1865c461d37e60c7083e8db9ad40c4bed98c')
-        new File(pluginDir, '5c9f1865c461d37e60c7083e8db9ad40c4bed98c').list().contains('org.intellij.plugins.markdown-201.6668.74.zip')
-        new File(pluginsCacheDir, 'unzipped.com.jetbrains.plugins').list().contains('org.intellij.plugins.markdown-201.6668.74')
+        def pluginDir = new File(pluginsRepoCacheDir, 'org.intellij.plugins.markdown/201.6668.74')
+        pluginDir.list().contains('17328855fcd031f39a805db934c121eaa25dedfb')
+        new File(pluginDir, '17328855fcd031f39a805db934c121eaa25dedfb').list().contains('org.intellij.plugins.markdown-201.6668.74.zip')
+        pluginsCacheDir.list().contains('org.intellij.plugins.markdown-201.6668.74')
     }
 
     def 'download jar plugin'() {
@@ -48,9 +52,9 @@ class DownloadIntelliJPluginsSpec extends IntelliJPluginSpecBase {
         build(BasePlugin.ASSEMBLE_TASK_NAME)
 
         then:
-        def pluginDir = new File(mavenCacheDir, 'com.jetbrains.plugins/org.jetbrains.postfixCompletion/0.8-beta')
-        pluginDir.list().contains('4c0b55a3dea095dd8e085b1c65fcc7b917b74dd5')
-        new File(pluginDir, '4c0b55a3dea095dd8e085b1c65fcc7b917b74dd5').list().contains('org.jetbrains.postfixCompletion-0.8-beta.jar')
+        def pluginDir = new File(pluginsRepoCacheDir, 'org.jetbrains.postfixCompletion/0.8-beta')
+        pluginDir.list().contains('dd37fa3fb1ecbf3d1c2fdb0049ba821fd32f2565')
+        new File(pluginDir, 'dd37fa3fb1ecbf3d1c2fdb0049ba821fd32f2565').list().contains('org.jetbrains.postfixCompletion-0.8-beta.jar')
     }
 
     def 'download plugin from custom repository'() {
@@ -63,11 +67,11 @@ class DownloadIntelliJPluginsSpec extends IntelliJPluginSpecBase {
         build(BasePlugin.ASSEMBLE_TASK_NAME)
 
         then:
-        new File(pluginsCacheDir, 'unzipped.com.jetbrains.plugins').list().contains('com.intellij.plugins.emacskeymap-201.6251.22')
+        pluginsCacheDir.list().contains('com.intellij.plugins.emacskeymap-201.6251.22')
     }
 
     def 'download plugin from custom repository 2'() {
-        URL resource = this.getClass().getClassLoader().getResource("custom-repo/plugins.xml")
+        URL resource = this.getClass().getClassLoader().getResource("custom-repo-2/plugins.xml")
 
         given:
         buildFile << "intellij { pluginsRepo { custom('${resource}') }; plugins = [\"com.intellij.plugins.emacskeymap:201.6251.22\"] }"
@@ -76,6 +80,45 @@ class DownloadIntelliJPluginsSpec extends IntelliJPluginSpecBase {
         build(BasePlugin.ASSEMBLE_TASK_NAME)
 
         then:
-        new File(pluginsCacheDir, 'unzipped.com.jetbrains.plugins').list().contains('com.intellij.plugins.emacskeymap-201.6251.22')
+        pluginsCacheDir.list().contains('com.intellij.plugins.emacskeymap-201.6251.22')
+    }
+
+    def 'download plugin from custom repository with query'() {
+        URL resource = this.getClass().getClassLoader().getResource("custom-repo-2/plugins.xml")
+
+        given:
+        buildFile << "intellij { pluginsRepo { custom('${resource}?query=1') }; plugins = [\"com.intellij.plugins.emacskeymap:201.6251.22\"] }"
+
+        when:
+        build(BasePlugin.ASSEMBLE_TASK_NAME)
+
+        then:
+        pluginsCacheDir.list().contains('com.intellij.plugins.emacskeymap-201.6251.22')
+    }
+
+    def 'download plugin from custom repository without xml'() {
+        URL resource = this.getClass().getClassLoader().getResource("custom-repo")
+
+        given:
+        buildFile << "intellij { pluginsRepo { custom('${resource}') }; plugins = [\"com.intellij.plugins.emacskeymap:201.6251.22\"] }"
+
+        when:
+        build(BasePlugin.ASSEMBLE_TASK_NAME)
+
+        then:
+        pluginsCacheDir.list().contains('com.intellij.plugins.emacskeymap-201.6251.22')
+    }
+
+    def 'download plugin from custom repository without xml with query'() {
+        URL resource = this.getClass().getClassLoader().getResource("custom-repo")
+
+        given:
+        buildFile << "intellij { pluginsRepo { custom('${resource}?query=1') }; plugins = [\"com.intellij.plugins.emacskeymap:201.6251.22\"] }"
+
+        when:
+        build(BasePlugin.ASSEMBLE_TASK_NAME)
+
+        then:
+        pluginsCacheDir.list().contains('com.intellij.plugins.emacskeymap-201.6251.22')
     }
 }
