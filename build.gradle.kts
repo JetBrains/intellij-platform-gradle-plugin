0bc0a80336f261f02c4771aaf27440cce94c56e8
Alexander Zolotov <zolotov@jetbrains.com>
2021-04-22 16:43:28 +0200

    Publish java components in snapshot

--- build.gradle.kts
+++ build.gradle.kts
@@ -124,7 +124,7 @@ publishing {
             artifactId = project.name
             version = version.toString()
 
-            artifact(tasks.findByName("jar"))
+            from(components["java"])
             artifact(sourcesJar.get())
             artifact(javadocJar.get())
 
@@ -144,7 +144,7 @@ publishing {
                 licenses {
                     license {
                         name.set("The Apache License, Version 2.0")
-                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
                     }
                 }
 
