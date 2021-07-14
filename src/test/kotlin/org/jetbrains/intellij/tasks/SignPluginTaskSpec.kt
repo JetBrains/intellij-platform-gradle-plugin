2ba817bc836824cc73324ec24701fa66c28f4a01
Brian McNamara <brian.mcnamara@salesforce.com>
2021-07-14 12:34:40 -0700

    Add test

--- src/test/kotlin/org/jetbrains/intellij/tasks/SignPluginTaskSpec.kt
+++ src/test/kotlin/org/jetbrains/intellij/tasks/SignPluginTaskSpec.kt
@@ -2,6 +2,7 @@ package org.jetbrains.intellij.tasks
 
 import org.jetbrains.intellij.IntelliJPluginConstants
 import org.jetbrains.intellij.IntelliJPluginSpecBase
+import java.io.File
 import kotlin.test.Test
 import kotlin.test.assertTrue
 
@@ -13,5 +14,18 @@ class SignPluginTaskSpec : IntelliJPluginSpecBase() {
         val result = build(IntelliJPluginConstants.SIGN_PLUGIN_TASK_NAME, "--configuration-cache")
 
         assertTrue(result.output.contains("Reusing configuration cache."))
+    }
+
+    @Test
+    fun `output file contains version when specified in build file`() {
+        buildFile.appendText("version '1.0.0' \n" +
+                "signPlugin {" +
+                "    certificateChain.set(provider { file(\"${javaClass.classLoader.getResource("certificates/cert.crt").file}\").text })\n" +
+                "    privateKey.set(provider { file(\"${javaClass.classLoader.getResource("certificates/cert.key").file}\").text })\n" +
+                "}")
+        build(IntelliJPluginConstants.SIGN_PLUGIN_TASK_NAME)
+
+        val distributionFolder = File(buildDirectory, "distributions")
+        assertTrue(distributionFolder.listFiles().asList().any() { it.name.contains("1.0.0-signed.zip") })
     }
 }
