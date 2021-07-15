0877adbb62e1ffceac9f0fecce37fa40e68ca873
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2021-07-15 22:48:50 +0200

    SignPluginTaskSpec code review

--- src/test/kotlin/org/jetbrains/intellij/tasks/SignPluginTaskSpec.kt
+++ src/test/kotlin/org/jetbrains/intellij/tasks/SignPluginTaskSpec.kt
@@ -9,6 +9,23 @@ import kotlin.test.assertTrue
 class SignPluginTaskSpec : IntelliJPluginSpecBase() {
 
     @Test
+    fun `output file contains version when specified in build file`() {
+        buildFile.groovy("""
+            version '1.0.0'
+            signPlugin {
+                certificateChain.set(file("${loadCertFile("certificates/cert.crt")}").text)
+                privateKey.set(file("${loadCertFile("certificates/cert.key")}").text)
+            }
+        """)
+        build(IntelliJPluginConstants.SIGN_PLUGIN_TASK_NAME)
+
+        val distributionFolder = File(buildDirectory, "distributions")
+        assertTrue(distributionFolder.listFiles()?.asList()?.any {
+            it.name.equals("projectName-1.0.0-signed.zip")
+        } ?: false)
+    }
+
+    @Test
     fun `reuse configuration cache`() {
         build(IntelliJPluginConstants.SIGN_PLUGIN_TASK_NAME, "--configuration-cache")
         val result = build(IntelliJPluginConstants.SIGN_PLUGIN_TASK_NAME, "--configuration-cache")
@@ -16,16 +33,5 @@ class SignPluginTaskSpec : IntelliJPluginSpecBase() {
         assertTrue(result.output.contains("Reusing configuration cache."))
     }
 
-    @Test
-    fun `output file contains version when specified in build file`() {
-        buildFile.appendText("version '1.0.0' \n" +
-                "signPlugin {" +
-                "    certificateChain.set(provider { file(\"${javaClass.classLoader.getResource("certificates/cert.crt").file}\").text })\n" +
-                "    privateKey.set(provider { file(\"${javaClass.classLoader.getResource("certificates/cert.key").file}\").text })\n" +
-                "}")
-        build(IntelliJPluginConstants.SIGN_PLUGIN_TASK_NAME)
-
-        val distributionFolder = File(buildDirectory, "distributions")
-        assertTrue(distributionFolder.listFiles().asList().any { it.name.contains("1.0.0-signed.zip") })
-    }
+    private fun loadCertFile(name: String) = javaClass.classLoader.getResource(name)?.file
 }
