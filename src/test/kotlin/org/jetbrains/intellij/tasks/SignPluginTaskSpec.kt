2ac2397a13ecdf6574491393eb9c3d77fecc3993
Brian McNamara <brianmay27@gmail.com>
2021-07-15 13:49:31 -0700

    Move signPlugin file name creation to lazy (#742)
    * Move signPlugin file name creation to lazy
    
    * Add test
    
    * Style check
    
    * SignPluginTaskSpec code review
    
    Co-authored-by: Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>

--- src/test/kotlin/org/jetbrains/intellij/tasks/SignPluginTaskSpec.kt
+++ src/test/kotlin/org/jetbrains/intellij/tasks/SignPluginTaskSpec.kt
@@ -2,10 +2,28 @@ package org.jetbrains.intellij.tasks
 
 import org.jetbrains.intellij.IntelliJPluginConstants
 import org.jetbrains.intellij.IntelliJPluginSpecBase
+import java.io.File
 import kotlin.test.Test
 import kotlin.test.assertTrue
 
 class SignPluginTaskSpec : IntelliJPluginSpecBase() {
+
+    @Test
+    fun `output file contains version when specified in build file`() {
+        buildFile.groovy("""
+            version '1.0.0'
+            signPlugin {
+                certificateChain.set(file("${loadCertFile("certificates/cert.crt")}").text)
+                privateKey.set(file("${loadCertFile("certificates/cert.key")}").text)
+            }
+        """)
+        build(IntelliJPluginConstants.SIGN_PLUGIN_TASK_NAME)
+
+        val distributionFolder = File(buildDirectory, "distributions")
+        assertTrue(distributionFolder.listFiles()?.asList()?.any {
+            it.name.equals("projectName-1.0.0-signed.zip")
+        } ?: false)
+    }
 
     @Test
     fun `reuse configuration cache`() {
@@ -14,4 +32,6 @@ class SignPluginTaskSpec : IntelliJPluginSpecBase() {
 
         assertTrue(result.output.contains("Reusing configuration cache."))
     }
+
+    private fun loadCertFile(name: String) = javaClass.classLoader.getResource(name)?.file
 }
