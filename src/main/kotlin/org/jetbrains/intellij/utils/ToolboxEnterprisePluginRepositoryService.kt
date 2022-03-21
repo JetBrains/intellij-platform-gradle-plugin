448da09f239a951ae60adb1e7a2e4b4033a49f64
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2022-03-21 12:26:18 +0100

    Code review fixes

--- src/main/kotlin/org/jetbrains/intellij/utils/ToolboxEnterprisePluginRepositoryService.kt
+++ src/main/kotlin/org/jetbrains/intellij/utils/ToolboxEnterprisePluginRepositoryService.kt
@@ -3,18 +3,16 @@ package org.jetbrains.intellij.utils
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import org.jetbrains.intellij.pluginRepository.internal.api.PluginRepositoryService
-import org.jetbrains.intellij.pluginRepository.model.PluginBean
 import org.jetbrains.intellij.pluginRepository.model.PluginUpdateBean
 import retrofit2.Call
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
-import retrofit2.http.Path
 
 interface ToolboxEnterprisePluginRepositoryService : PluginRepositoryService {
 
     @Multipart
-    @POST("/plugin/uploadPlugin")
+    @POST("/api/ij-plugins/upload")
     override fun upload(
         @Part("pluginId") pluginId: Int,
         @Part("channel") channel: RequestBody?,
@@ -23,20 +21,11 @@ interface ToolboxEnterprisePluginRepositoryService : PluginRepositoryService {
     ): Call<PluginUpdateBean>
 
     @Multipart
-    @POST("/plugin/uploadPlugin")
+    @POST("/api/ij-plugins/upload")
     override fun uploadByXmlId(
         @Part("xmlId") pluginXmlId: RequestBody,
         @Part("channel") channel: RequestBody?,
         @Part("notes") notes: RequestBody?,
         @Part file: MultipartBody.Part
     ): Call<PluginUpdateBean>
-
-    @Multipart
-    @POST("/api/plugins/{family}/upload")
-    override fun uploadNewPlugin(
-        @Part file: MultipartBody.Part,
-        @Path("family") family: String,
-        @Part("licenseUrl") licenseUrl: RequestBody,
-        @Part("cid") category: Int
-    ): Call<PluginBean>
 }
