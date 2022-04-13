38adfecff8f55a9170ee2f78116850ca03fff39c
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2022-04-13 09:52:56 +0200

    221

--- integration-tests/plugin-xml-patching/verify.main.kts
+++ integration-tests/plugin-xml-patching/verify.main.kts
@@ -7,4 +7,4 @@ logs matchesRegex ":plugin-xml-patching:patchPluginXml .*? completed."
 buildDirectory containsFile "patchedPluginXmlFiles/plugin.xml"
 
 patchedPluginXml containsText "<version>1.0.0</version>"
-patchedPluginXml containsText "<idea-version since-build=\"211\" until-build=\"213.*\" />"
+patchedPluginXml containsText "<idea-version since-build=\"221\" until-build=\"221.*\" />"
