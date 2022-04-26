eeaa731dd314294bbb6f7045359248e523c3de03
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2022-04-26 09:54:49 +0200

    Migrate `ToolboxEnterprisePluginRepositoryService` to use JSON API instead of deprecated methods

--- src/main/kotlin/org/jetbrains/intellij/utils/ToolboxEnterprisePluginRepositoryService.kt
+++ src/main/kotlin/org/jetbrains/intellij/utils/ToolboxEnterprisePluginRepositoryService.kt
@@ -2,8 +2,8 @@ package org.jetbrains.intellij.utils
 
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
-import okhttp3.ResponseBody
 import org.jetbrains.intellij.pluginRepository.internal.api.PluginRepositoryService
+import org.jetbrains.intellij.pluginRepository.model.PluginUpdateBean
 import retrofit2.Call
 import retrofit2.http.Multipart
 import retrofit2.http.POST
@@ -11,23 +11,21 @@ import retrofit2.http.Part
 
 interface ToolboxEnterprisePluginRepositoryService : PluginRepositoryService {
 
-    @Deprecated("Use JSON API")
     @Multipart
     @POST("/api/ij-plugins/upload")
-    override fun upload(
+    override fun uploadById(
         @Part("pluginId") pluginId: Int,
         @Part("channel") channel: RequestBody?,
         @Part("notes") notes: RequestBody?,
         @Part file: MultipartBody.Part
-    ): Call<ResponseBody>
+    ): Call<PluginUpdateBean>
 
-    @Deprecated("Use JSON API")
     @Multipart
     @POST("/api/ij-plugins/upload")
-    override fun uploadByXmlId(
-        @Part("xmlId") pluginXmlId: RequestBody,
-        @Part("channel") channel: RequestBody?,
-        @Part("notes") notes: RequestBody?,
-        @Part file: MultipartBody.Part
-    ): Call<ResponseBody>
+    override fun uploadByStringId(
+        pluginXmlId: RequestBody,
+        channel: RequestBody?,
+        notes: RequestBody?,
+        file: MultipartBody.Part
+    ): Call<PluginUpdateBean>
 }
