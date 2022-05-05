682540152eb55669d0f81e450944c26365b21c2a
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2022-05-05 21:35:42 +0200

    Integration Tests: Remove copyrights header from `verify.main.kts`

--- integration-tests/build-features/verify.main.kts
+++ integration-tests/build-features/verify.main.kts
@@ -1,5 +1,3 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-
 #!/usr/bin/env -S kotlin -J-ea
 
 @file:Import("../verify.utils.kts")
