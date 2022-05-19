97a09219d8876180902b6825f1864462158a7a28
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2022-05-19 09:07:20 +0200

    x

--- src/test/kotlin/org/jetbrains/intellij/tasks/DownloadIntelliJPluginsSpec.kt
+++ src/test/kotlin/org/jetbrains/intellij/tasks/DownloadIntelliJPluginsSpec.kt
@@ -47,6 +47,8 @@ class DownloadIntelliJPluginsSpec : IntelliJPluginSpecBase() {
         build(BasePlugin.ASSEMBLE_TASK_NAME)
         val pluginDir = File(pluginsRepositoryCacheDir, "com.intellij.lang.jsgraphql/2.9.1")
         println("pluginDir.list()='${pluginDir.list()?.joinToString()}'")
+
+        throw Exception("pluginDir.list()='${pluginDir.list()?.joinToString()}'")
         pluginDir.list()?.let {
             assertTrue(it.contains("941149d2af84d374af5571b29469fd97ac6a9f6e"))
         }
