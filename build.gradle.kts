7ac024ab3d6c56d5b19d00d995b48f76119566af
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2022-10-29 23:15:27 +0200

    dependencies update

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,10 +32,10 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains:annotations:23.0.0")
-    implementation("org.jetbrains.intellij.plugins:structure-base:3.238") {
+    implementation("org.jetbrains.intellij.plugins:structure-base:3.239") {
         exclude("org.jetbrains.kotlin")
     }
-    implementation("org.jetbrains.intellij.plugins:structure-intellij:3.238") {
+    implementation("org.jetbrains.intellij.plugins:structure-intellij:3.239") {
         exclude("org.jetbrains.kotlin")
     }
     implementation("org.jetbrains.intellij:plugin-repository-rest-client:2.0.28") {
@@ -44,7 +44,7 @@ dependencies {
     }
 
     implementation("com.fasterxml.jackson.core:jackson-databind:2.13.4.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
     implementation("javax.xml.bind:jaxb-api:2.4.0-b180830.0359")
     implementation("com.googlecode.plist:dd-plist:1.25")
 
