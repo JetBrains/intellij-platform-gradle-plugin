93e1c4a326fc1401a23e5a52671c97a97f716df9
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2022-12-01 14:54:58 +0100

    Bump Kotlin libraries to 1.7.22

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,7 +52,7 @@ dependencies {
     implementation("com.googlecode.plist:dd-plist:1.26")
 
     api("gradle.plugin.org.jetbrains.gradle.plugin.idea-ext:gradle-idea-ext:1.1.6")
-    api("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
+    api("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.22")
     api("com.squareup.retrofit2:retrofit:2.9.0")
 
     testImplementation(gradleTestKit())
