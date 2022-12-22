c3042c5a25abfbdf756e20635ef14fe113d696ec
hsz <hsz@TeamCity>
2022-12-22 12:03:11 +0000

    TeamCity change in 'Open-source projects / Gradle IntelliJ Plugin' project: runners of 'Unit Tests' build configuration were updated

--- .teamcity/patches/buildTypes/UnitTests.kts
+++ .teamcity/patches/buildTypes/UnitTests.kts
@@ -1,8 +1,6 @@
 package patches.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.*
-import jetbrains.buildServer.configs.kotlin.buildSteps.gradle
-import jetbrains.buildServer.configs.kotlin.buildSteps.maven
 import jetbrains.buildServer.configs.kotlin.ui.*
 
 /*
@@ -11,30 +9,6 @@ To apply the patch, change the buildType with id = 'UnitTests'
 accordingly, and delete the patch script.
 */
 changeBuildType(RelativeId("UnitTests")) {
-    expectSteps {
-        gradle {
-            name = "Unit Tests – Gradle 6.7.1"
-            tasks = "check -PtestGradleVersion=6.7.1"
-        }
-        gradle {
-            name = "Unit Tests – Gradle 6.9.2"
-            tasks = "check -PtestGradleVersion=6.9.2"
-        }
-        gradle {
-            name = "Unit Tests – Gradle 7.5.1"
-            tasks = "check -PtestGradleVersion=7.5.1"
-        }
-    }
-    steps {
-        insert(3) {
-            maven {
-                goals = "clean test"
-                pomLocation = ".teamcity/pom.xml"
-                runnerArgs = "-Dmaven.test.failure.ignore=true"
-            }
-        }
-    }
-
     requirements {
         add {
             equals("teamcity.agent.jvm.os.name", "Linux")
