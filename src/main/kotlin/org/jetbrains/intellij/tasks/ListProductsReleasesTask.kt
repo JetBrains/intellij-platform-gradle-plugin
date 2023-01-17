df9094fcd53291dfbe15d7f7f728b47162a1d3b0
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2023-01-17 21:13:56 +0100

    Fixed tests

--- src/main/kotlin/org/jetbrains/intellij/tasks/ListProductsReleasesTask.kt
+++ src/main/kotlin/org/jetbrains/intellij/tasks/ListProductsReleasesTask.kt
@@ -157,7 +157,7 @@ abstract class ListProductsReleasesTask : DefaultTask() {
             .flatMap { product -> product.codes.map { it to product }.asSequence() }
             .filter { (type) -> types.contains(type) }
             .flatMap { (type, product) -> product.channels.map { type to it }.asSequence() }
-            .filter { (_, channel) -> channels.contains(Channel.valueOf(channel.status.replaceFirstChar(Char::uppercase))) }
+            .filter { (_, channel) -> channels.contains(Channel.valueOf(channel.status.uppercase())) }
             .flatMap { (type, channel) ->
                 channel.builds.map {
                     type to (it.version.run(Version::parse) to it.number.run(Version::parse))
@@ -181,7 +181,7 @@ abstract class ListProductsReleasesTask : DefaultTask() {
                     val build = item.platformBuild?.let(Version::parse)
                     testVersion(version, build)
                 }
-                .filter { channels.contains(Channel.valueOf(it.channel.replaceFirstChar(Char::uppercase))) }
+                .filter { channels.contains(Channel.valueOf(it.channel.uppercase())) }
                 .groupBy { it.version.split('.').dropLast(1).joinToString(".") }
                 .mapNotNull { entry ->
                     entry.value.maxByOrNull {
