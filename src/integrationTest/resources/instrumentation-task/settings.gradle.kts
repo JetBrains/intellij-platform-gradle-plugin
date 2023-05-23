8aeefc76f9ed6d5a440a151c5616c184fd27c9a8
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2023-05-23 16:59:08 +0200

    Integration Tests: Added missing `kotlin.jvmToolchain(11)` to the `instrumentation-task` submodule

--- src/integrationTest/resources/instrumentation-task/settings.gradle.kts
+++ src/integrationTest/resources/instrumentation-task/settings.gradle.kts
@@ -1,4 +1,3 @@
 rootProject.name = "test"
 
 include(":submodule")
-//includeBuild("/Users/hsz/Projects/JetBrains/gradle-intellij-plugin")
--- src/integrationTest/resources/instrumentation-task/submodule/build.gradle.kts
+++ src/integrationTest/resources/instrumentation-task/submodule/build.gradle.kts
@@ -8,6 +8,10 @@ repositories {
     mavenCentral()
 }
 
+kotlin {
+    jvmToolchain(11)
+}
+
 intellij {
     version.set("2022.1.4")
 }
