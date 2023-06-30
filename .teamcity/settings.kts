7c958134b4d1a47bd780935f28786bc7d7da596c
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2023-06-30 21:36:26 +0200

    Add Gradle `8.2-rc-3` to the testing matrix

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -44,7 +44,7 @@ project {
     }
 
     val operatingSystems = listOf("Linux", "Windows", "macOS")
-    val gradleVersions = listOf("7.6", "8.1.1")
+    val gradleVersions = listOf("7.6", "8.1.1", "8.2-rc-3")
 
     val buildChain = sequential {
         operatingSystems.forEach { os ->
