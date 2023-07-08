70e5e467cb1a20b9b0691eb8b0ae3fa4bb38ba6f
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-07-08 11:00:15 +0100

    Remove redundant Gradle version from CI

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -44,7 +44,7 @@ project {
     }
 
     val operatingSystems = listOf("Linux", "Windows", "macOS")
-    val gradleVersions = listOf("7.6", "8.2", "8.2-rc-3")
+    val gradleVersions = listOf("7.6", "8.2")
 
     val buildChain = sequential {
         operatingSystems.forEach { os ->
