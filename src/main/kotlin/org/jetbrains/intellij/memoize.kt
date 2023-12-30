0c003176c25b72cfaef02d6f084d15995efe961b
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-12-30 18:39:01 +0000

    Change formatting

--- src/main/kotlin/org/jetbrains/intellij/memoize.kt
+++ src/main/kotlin/org/jetbrains/intellij/memoize.kt
@@ -8,15 +8,16 @@ import org.gradle.api.internal.provider.ProviderInternal
 import org.gradle.api.internal.provider.ValueSupplier
 import org.gradle.api.provider.Provider
 
-internal fun <T> Provider<T>.memoize(): Provider<T> = when (this) {
-    // ValueSource instances already memoize their value
-    is DefaultValueSourceProviderFactory.ValueSourceProvider<*, *> -> this
-
-    // cannot memoize something that isn't a ProviderInternal; pretty much everything *must*
-    // be a ProviderInternal, as this is how internal state (dependencies, etc) are carried
-    !is ProviderInternal<T> -> error("Expected ProviderInternal, got $this")
-    else -> MemoizedProvider(this)
-}
+internal fun <T> Provider<T>.memoize(): Provider<T> =
+    when (this) {
+        // ValueSource instances already memoize their value
+        is DefaultValueSourceProviderFactory.ValueSourceProvider<*, *> -> this
+    
+        // cannot memoize something that isn't a ProviderInternal; pretty much everything *must*
+        // be a ProviderInternal, as this is how internal state (dependencies, etc) are carried
+        !is ProviderInternal<T> -> error("Expected ProviderInternal, got $this")
+        else -> MemoizedProvider(this)
+    }
 
 private class MemoizedProvider<T>(private val delegate: ProviderInternal<T>) : AbstractMinimalProvider<T>() {
 
