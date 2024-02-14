8e9c2f2291e521edea08906db46bbd215b3ea309
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2024-02-14 12:01:02 +0100

    IntelliJPlatformMigrationPlugin cleanup

--- src/main/kotlin/org/jetbrains/intellij/platform/gradle/plugins/project/IntelliJPlatformMigrationPlugin.kt
+++ src/main/kotlin/org/jetbrains/intellij/platform/gradle/plugins/project/IntelliJPlatformMigrationPlugin.kt
@@ -12,54 +12,43 @@ import org.jetbrains.intellij.platform.gradle.plugins.configureExtension
 import org.jetbrains.intellij.platform.gradle.utils.Logger
 
 private const val PLUGIN_MIGRATION_ID = "$PLUGIN_ID.migration"
-private const val OLD_INTELLIJ_EXTENSION = "intellij"
+
+// language=TEXT
+private object Messages {
+    const val DEPRECATED_INTELLIJ_PLUGIN_NAME =
+        "\nThe `intellij.pluginName` property is no longer available.\nUse: `intellijPlatform.pluginConfiguration.name` instead:\n\nintellijPlatform {\n    pluginConfiguration {\n        name = ...\n    }\n}\n\nSee: $DOCS/tools-intellij-platform-gradle-plugin-extension.html#intellijPlatform-pluginConfiguration\nMigration guide: $DOCS/tools-intellij-platform-gradle-plugin-migration.html"
+    const val DEPRECATED_INTELLIJ_TYPE_VERSION =
+        "\nThe `intellij.type` and `intellij.version` properties are no longer available.\nDefine the IntelliJ Platform dependency in `dependencies {}` block instead:\n\nrepositories {\n    mavenCentral()\n    intellijPlatform {\n        defaultRepositories()\n    }\n}\n\ndependencies {\n    intellijPlatform {\n        create(type, version)\n    }\n}\n\nSee: $DOCS/tools_intellij_platform_gradle_plugin_extension.md#intellijPlatform-pluginConfiguration-name\nMigration guide: $DOCS/tools-intellij-platform-gradle-plugin-migration.html"
+    const val DEPRECATED_INTELLIJ_PLUGINS =
+        "\nThe `intellij.plugins` property is no longer available.\nDefine dependencies on plugins or bundled plugins in `dependencies {}` block instead:\n\nrepositories {\n    mavenCentral()\n    intellijPlatform {\n        defaultRepositories()\n    }\n}\n\ndependencies {\n    intellijPlatform {\n        plugins(properties(\"platformPlugins\").map { it.split(',') })\n        bundledPlugins(properties(\"platformBundledPlugins\").map { it.split(',') })\n    }\n}\n\nNote that bundled plugins are now separated from plugins available in JetBrains Marketplace.\n\nSee: $DOCS/tools-intellij-platform-gradle-plugin-dependencies-extension.html\nMigration guide: $DOCS/tools-intellij-platform-gradle-plugin-migration.html\n"
+    const val MIGRATION_PLUGIN_ENABLED =
+        "You are currently using the $PLUGIN_MIGRATION_ID which is dedicated only to help with the migration process from the Gradle IntelliJ Plugin 1.x to IntelliJ Platform Gradle Plugin 2.0.\nAs soon as you'll handle all configuration problems, remember to remove this plugin from your `plugins {}` block.\n\nNote: We encourage avoiding configuring tasks directly, like: patchPluginXml { ... }, publishPlugin { ... }, signPlugin { ... }, ...\n      Use the `intellijPlatform { }` extension instead:\n      \nintellijPlatform {\n    ...\n    \n    pluginConfiguration { ... }\n    publishing { ... }\n    signing { ... }\n    ...\n}\n\nSee: $DOCS/intellij-platform-gradle-plugin-extension-plugin.xml\nMigration guide: $DOCS/tools-intellij-platform-gradle-plugin-migration.html"
+}
 
 @Suppress("unused")
 abstract class IntelliJPlatformMigrationPlugin : Plugin<Project> {
 
     private val log = Logger(javaClass)
-
     override fun apply(project: Project) {
         log.info("Configuring plugin: $PLUGIN_MIGRATION_ID")
+        log.error(Messages.MIGRATION_PLUGIN_ENABLED)
 
-        project.configureExtension<IntelliJExtension>(OLD_INTELLIJ_EXTENSION)
-
-        log.error("""
-        You are currently using the $PLUGIN_MIGRATION_ID which is dedicated only to help with the migration process from the Gradle IntelliJ Plugin 1.x to IntelliJ Platform Gradle Plugin 2.0.
-        As soon as you'll handle all configuration problems, remember to remove this plugin from your `plugins {}` block.
-        
-        Note: we encourage to avoid configuring tasks, such as `patchPluginXml`, `publishPlugin`, `signPlugin`, etc. manually but use the `intellijPlatform {}` extension instead.
-        
-        See: $DOCS/intellij-platform-gradle-plugin-extension-plugin.xml
-        Migration guide: $DOCS/tools-intellij-platform-gradle-plugin-migration.html
-        """.trimIndent())
+        project.configureExtension<IntelliJExtension>("intellij")
     }
 }
 
-// language=TEXT
 interface IntelliJExtension {
 
-    @Deprecated(
-        message = "\nThe `intellij.pluginName` property is no longer available.\nUse: `intellijPlatform.pluginConfiguration.name` instead:\n\nintellijPlatform {\n    pluginConfiguration {\n        name = ...\n    }\n}\n\nSee: $DOCS/tools-intellij-platform-gradle-plugin-extension.html#intellijPlatform-pluginConfiguration\nMigration guide: $DOCS/tools-intellij-platform-gradle-plugin-migration.html\n",
-        level = DeprecationLevel.ERROR,
-    )
+    @Deprecated(Messages.DEPRECATED_INTELLIJ_PLUGIN_NAME, level = DeprecationLevel.ERROR)
     val pluginName: Property<String>
 
-    @Deprecated(
-        message = "\nThe `intellij.version` property is no longer available.\nDefine the IntelliJ Platform dependency in `dependencies {}` block instead:\n\nrepositories {\n    mavenCentral()\n    intellijPlatform {\n        defaultRepositories()\n    }\n}\n\ndependencies {\n    intellijPlatform {\n        create(type, version)\n    }\n}\n\nSee: $DOCS/tools_intellij_platform_gradle_plugin_extension.md#intellijPlatform-pluginConfiguration-name\nMigration guide: $DOCS/tools-intellij-platform-gradle-plugin-migration.html\n",
-        level = DeprecationLevel.ERROR,
-    )
+    @Deprecated(Messages.DEPRECATED_INTELLIJ_TYPE_VERSION, level = DeprecationLevel.ERROR)
     val version: Property<String>
 
-    @Deprecated(
-        message = "\nThe `intellij.type` property is no longer available.\nDefine the IntelliJ Platform dependency in `dependencies {}` block instead:\n\nrepositories {\n    mavenCentral()\n    intellijPlatform {\n        defaultRepositories()\n    }\n}\n\ndependencies {\n    intellijPlatform {\n        create(type, version)\n    }\n}\n\nSee: $DOCS/tools-intellij-platform-gradle-plugin-dependencies-extension.html\nMigration guide: $DOCS/tools-intellij-platform-gradle-plugin-migration.html\n",
-        level = DeprecationLevel.ERROR,
-    )
+    @Deprecated(Messages.DEPRECATED_INTELLIJ_TYPE_VERSION, level = DeprecationLevel.ERROR)
     val type: Property<String>
 
-    @Deprecated(
-        message = "\nThe `intellij.plugins` property is no longer available.\nDefine dependencies on plugins or bundled plugins in `dependencies {}` block instead:\n\nrepositories {\n    mavenCentral()\n    intellijPlatform {\n        defaultRepositories()\n    }\n}\n\ndependencies {\n    intellijPlatform {\n        plugins(properties(\"platformPlugins\").map { it.split(',') })\n        bundledPlugins(properties(\"platformBundledPlugins\").map { it.split(',') })\n    }\n}\n\nNote that bundled plugins are now separated from plugins available in JetBrains Marketplace.\n\nSee: $DOCS/tools-intellij-platform-gradle-plugin-dependencies-extension.html\nMigration guide: $DOCS/tools-intellij-platform-gradle-plugin-migration.html\n",
-        level = DeprecationLevel.ERROR,
-    )
+    @Deprecated(Messages.DEPRECATED_INTELLIJ_PLUGINS, level = DeprecationLevel.ERROR)
     val plugins: ListProperty<Any>
+
 }
