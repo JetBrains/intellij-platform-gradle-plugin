521f73d5cb435074af3590e71ac9ca206ab5e5f9
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2024-02-15 11:42:54 +0100

    IntelliJPlatformMigrationPlugin: introduced more cases

--- src/main/kotlin/org/jetbrains/intellij/platform/gradle/plugins/project/IntelliJPlatformMigrationPlugin.kt
+++ src/main/kotlin/org/jetbrains/intellij/platform/gradle/plugins/project/IntelliJPlatformMigrationPlugin.kt
@@ -1,28 +1,78 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
+@file:Suppress("ConstPropertyName", "unused", "DeprecatedCallableAddReplaceWith")
+
 package org.jetbrains.intellij.platform.gradle.plugins.project
 
+import org.gradle.api.DefaultTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.provider.ListProperty
 import org.gradle.api.provider.Property
+import org.gradle.kotlin.dsl.register
 import org.jetbrains.intellij.platform.gradle.IntelliJPluginConstants.Locations.DOCS
 import org.jetbrains.intellij.platform.gradle.IntelliJPluginConstants.PLUGIN_ID
+import org.jetbrains.intellij.platform.gradle.extensions.IntelliJPlatformExtension
 import org.jetbrains.intellij.platform.gradle.plugins.configureExtension
 import org.jetbrains.intellij.platform.gradle.utils.Logger
 
 private const val PLUGIN_MIGRATION_ID = "$PLUGIN_ID.migration"
 
 // language=TEXT
+private object Docs {
+    const val migration = "Migration guide: $DOCS/tools-intellij-platform-gradle-plugin-migration.html"
+    const val extension = "IntelliJ Platform Extension: $DOCS/tools-intellij-platform-gradle-plugin-extension.html"
+    const val dependencies = "IntelliJ Platform Extension: $DOCS/tools-intellij-platform-gradle-plugin-dependencies-extension.html"
+    const val repositories = "IntelliJ Platform Repositories Extension: $DOCS/tools-intellij-platform-gradle-plugin-repositories-extension.html"
+}
+
+// language=TEXT
 private object Messages {
-    const val DEPRECATED_INTELLIJ_PLUGIN_NAME =
-        "\nThe `intellij.pluginName` property is no longer available.\nUse: `intellijPlatform.pluginConfiguration.name` instead:\n\nintellijPlatform {\n    pluginConfiguration {\n        name = ...\n    }\n}\n\nSee: $DOCS/tools-intellij-platform-gradle-plugin-extension.html#intellijPlatform-pluginConfiguration\nMigration guide: $DOCS/tools-intellij-platform-gradle-plugin-migration.html"
-    const val DEPRECATED_INTELLIJ_TYPE_VERSION =
-        "\nThe `intellij.type` and `intellij.version` properties are no longer available.\nDefine the IntelliJ Platform dependency in `dependencies {}` block instead:\n\nrepositories {\n    mavenCentral()\n    intellijPlatform {\n        defaultRepositories()\n    }\n}\n\ndependencies {\n    intellijPlatform {\n        create(type, version)\n    }\n}\n\nSee: $DOCS/tools_intellij_platform_gradle_plugin_extension.md#intellijPlatform-pluginConfiguration-name\nMigration guide: $DOCS/tools-intellij-platform-gradle-plugin-migration.html"
-    const val DEPRECATED_INTELLIJ_PLUGINS =
-        "\nThe `intellij.plugins` property is no longer available.\nDefine dependencies on plugins or bundled plugins in `dependencies {}` block instead:\n\nrepositories {\n    mavenCentral()\n    intellijPlatform {\n        defaultRepositories()\n    }\n}\n\ndependencies {\n    intellijPlatform {\n        plugins(properties(\"platformPlugins\").map { it.split(',') })\n        bundledPlugins(properties(\"platformBundledPlugins\").map { it.split(',') })\n    }\n}\n\nNote that bundled plugins are now separated from plugins available in JetBrains Marketplace.\n\nSee: $DOCS/tools-intellij-platform-gradle-plugin-dependencies-extension.html\nMigration guide: $DOCS/tools-intellij-platform-gradle-plugin-migration.html\n"
-    const val MIGRATION_PLUGIN_ENABLED =
-        "You are currently using the $PLUGIN_MIGRATION_ID which is dedicated only to help with the migration process from the Gradle IntelliJ Plugin 1.x to IntelliJ Platform Gradle Plugin 2.0.\nAs soon as you'll handle all configuration problems, remember to remove this plugin from your `plugins {}` block.\n\nNote: We encourage avoiding configuring tasks directly, like: patchPluginXml { ... }, publishPlugin { ... }, signPlugin { ... }, ...\n      Use the `intellijPlatform { }` extension instead:\n      \nintellijPlatform {\n    ...\n    \n    pluginConfiguration { ... }\n    publishing { ... }\n    signing { ... }\n    ...\n}\n\nSee: $DOCS/intellij-platform-gradle-plugin-extension-plugin.xml\nMigration guide: $DOCS/tools-intellij-platform-gradle-plugin-migration.html"
+    object IntelliJ {
+        const val plugins =
+            "Define dependencies on plugins or bundled plugins in `dependencies {}` block instead:\n\nrepositories {\n    mavenCentral()\n    intellijPlatform {\n        defaultRepositories()\n    }\n}\n\ndependencies {\n    intellijPlatform {\n        plugins(properties(\"platformPlugins\").map { it.split(',') })\n        bundledPlugins(properties(\"platformBundledPlugins\").map { it.split(',') })\n    }\n}\n\nNote that bundled plugins are now separated from plugins available in JetBrains Marketplace.\n\n${Docs.extension}\n${Docs.migration}"
+
+        const val localPath =
+            "Define dependencies on local IDE instance in `dependencies {}` block instead:\n\nrepositories {\n    mavenCentral()\n    intellijPlatform {\n        defaultRepositories()\n    }\n}\n\ndependencies {\n    intellijPlatform {\n        local(...)\n    }\n}\n\n${Docs.dependencies}\n${Docs.migration}"
+
+        const val localSourcesPath =
+            "Providing `localSourcesPath` is no longer available.\n\n${Docs.extension}\n${Docs.migration}"
+
+        const val typeVersion =
+            "Define the IntelliJ Platform dependency in `dependencies {}` block instead:\n\nrepositories {\n    mavenCentral()\n    intellijPlatform {\n        defaultRepositories()\n    }\n}\n\ndependencies {\n    intellijPlatform {\n        create(type, version)\n    }\n}\n\n${Docs.dependencies}\n${Docs.migration}"
+
+        const val pluginName =
+            "Use: `intellijPlatform.pluginConfiguration.name` instead:\n\nintellijPlatform {\n    pluginConfiguration {\n        name = ...\n    }\n}\n\n${Docs.extension}#intellijPlatform-pluginConfiguration\n${Docs.migration}"
+
+        const val sandboxDir =
+            "Use the `intellijPlatform.sandboxContainer`.\n\n${Docs.extension}\n${Docs.migration}"
+
+        const val downloadSources =
+            "Downloading sources is managed with the DevKit Plugin in version 2024.1+.\n\n${Docs.migration}"
+
+        const val ideaDependency =
+            "Access now the `ProductInfo` object using the `intellijPlatform.productInfo` property.\n\n${Docs.migration}"
+    }
+
+    object Tasks {
+        const val downloadRobotServerPlugin =
+            "The Robot Server Plugin integration is not yet available. Stay tuned!\n\n${Docs.migration}"
+
+        const val runIdeForUiTests =
+            "Use `testIdeUi` task.\n\n${Docs.migration}"
+    }
+
+    const val enabled =
+        "You are currently using the $PLUGIN_MIGRATION_ID which is dedicated only to help with the migration process from the Gradle IntelliJ Plugin 1.x to IntelliJ Platform Gradle Plugin 2.0.\nAs soon as you'll handle all configuration problems, remember to remove this plugin from your `plugins {}` block and eventially rename the `intellij {}` extension to `intellijPlatform {}`.\n\nNote: We encourage avoiding configuring tasks directly, like: patchPluginXml { ... }, publishPlugin { ... }, signPlugin { ... }, ...\n      Use the `intellijPlatform { }` extension instead:\n      \nintellijPlatform {\n    ...\n    \n    pluginConfiguration { ... }\n    publishing { ... }\n    signing { ... }\n    ...\n}\n\n${Docs.extension}\n${Docs.migration}"
+
+    const val sinceUntilBuild =
+        "The `plugin.xml` file is now fully managed by the `intellijPlatform` extension`.\n\n${Docs.extension}#intellijPlatform-pluginConfiguration-ideaVersion\n${Docs.migration}"
+
+    const val repositoryManagement =
+        "Use the `repositories {}` block to manage repositories instead.\n\n${Docs.repositories}\n${Docs.migration}"
+
+    const val verifyPlugin =
+        "Task responsible for running the IntelliJ Plugin Verifier is now called `verifyPlugin`.\n\nUse `intellijPlatform.verifyPlugin` extension to configure it.\n\n${Docs.repositories}#intellijPlatform-verifyPlugin\n${Docs.migration}"
 }
 
 @Suppress("unused")
@@ -32,23 +82,146 @@ abstract class IntelliJPlatformMigrationPlugin : Plugin<Project> {
 
     override fun apply(project: Project) {
         log.info("Configuring plugin: $PLUGIN_MIGRATION_ID")
-        log.error(Messages.MIGRATION_PLUGIN_ENABLED)
+        log.error(Messages.enabled)
 
         project.configureExtension<IntelliJExtension>("intellij")
+
+        with(project.tasks) {
+            register<DownloadRobotServerPluginTask>("downloadRobotServerPlugin")
+            register<RunIdeForUiTestsTask>("runIdeForUiTests")
+            register<RunPluginVerifierTask>("runPluginVerifier")
+        }
     }
 }
 
-interface IntelliJExtension {
+interface IntelliJExtension : IntelliJPlatformExtension {
 
-    @Deprecated(Messages.DEPRECATED_INTELLIJ_PLUGIN_NAME, level = DeprecationLevel.ERROR)
-    val pluginName: Property<String>
+    @Deprecated(Messages.IntelliJ.plugins, level = DeprecationLevel.ERROR)
+    val plugins: ListProperty<Any>
 
-    @Deprecated(Messages.DEPRECATED_INTELLIJ_TYPE_VERSION, level = DeprecationLevel.ERROR)
+    @Deprecated(Messages.IntelliJ.localPath, level = DeprecationLevel.ERROR)
+    val localPath: Property<String>
+
+    @Deprecated(Messages.IntelliJ.localSourcesPath, level = DeprecationLevel.ERROR)
+    val localSourcesPath: Property<String>
+
+    @Deprecated(Messages.IntelliJ.typeVersion, level = DeprecationLevel.ERROR)
     val version: Property<String>
 
-    @Deprecated(Messages.DEPRECATED_INTELLIJ_TYPE_VERSION, level = DeprecationLevel.ERROR)
+    @Deprecated(Messages.IntelliJ.typeVersion, level = DeprecationLevel.ERROR)
     val type: Property<String>
 
-    @Deprecated(Messages.DEPRECATED_INTELLIJ_PLUGINS, level = DeprecationLevel.ERROR)
-    val plugins: ListProperty<Any>
+    @Deprecated(Messages.IntelliJ.pluginName, level = DeprecationLevel.ERROR)
+    val pluginName: Property<String>
+
+    @Deprecated(Messages.sinceUntilBuild, level = DeprecationLevel.ERROR)
+    val updateSinceUntilBuild: Property<Boolean>
+
+    @Deprecated(Messages.sinceUntilBuild, level = DeprecationLevel.ERROR)
+    val sameSinceUntilBuild: Property<Boolean>
+
+    @Deprecated(Messages.IntelliJ.sandboxDir, level = DeprecationLevel.ERROR, replaceWith = ReplaceWith("sandboxContainer"))
+    val sandboxDir: Property<String>
+
+    @Deprecated(Messages.repositoryManagement, level = DeprecationLevel.ERROR)
+    val intellijRepository: Property<String>
+
+    @Deprecated(Messages.repositoryManagement, level = DeprecationLevel.ERROR)
+    val pluginsRepositories: Property<String>
+
+    @Deprecated(Messages.repositoryManagement, level = DeprecationLevel.ERROR)
+    val jreRepository: Property<String>
+
+    @Deprecated(Messages.IntelliJ.downloadSources, level = DeprecationLevel.ERROR)
+    val downloadSources: Property<Boolean>
+
+    @Deprecated(Messages.IntelliJ.ideaDependency, level = DeprecationLevel.ERROR, replaceWith = ReplaceWith("productInfo"))
+    val ideaDependency: Property<Any>
+}
+
+abstract class DownloadRobotServerPluginTask : DefaultTask() {
+
+    @Deprecated(Messages.Tasks.downloadRobotServerPlugin, level = DeprecationLevel.ERROR)
+    abstract val version: Property<String>
+}
+
+abstract class RunIdeForUiTestsTask : DefaultTask() {
+
+    @Deprecated(Messages.Tasks.runIdeForUiTests, level = DeprecationLevel.ERROR)
+    fun systemProperty(vararg arguments: Any) = Unit
+}
+
+abstract class RunPluginVerifierTask : DefaultTask() {
+
+    @Deprecated(Messages.verifyPlugin, level = DeprecationLevel.ERROR)
+    abstract val failureLevel: ListProperty<Any>
+
+    @Deprecated(Messages.verifyPlugin, level = DeprecationLevel.ERROR)
+    abstract val productsReleasesFile: Property<Any>
+
+    @Deprecated(Messages.verifyPlugin, level = DeprecationLevel.ERROR)
+    abstract val ideVersions: ListProperty<String>
+
+    @Deprecated(Messages.verifyPlugin, level = DeprecationLevel.ERROR)
+    abstract val localPaths: ListProperty<Any>
+
+    @Deprecated(Messages.verifyPlugin, level = DeprecationLevel.ERROR)
+    abstract val verifierVersion: Property<String>
+
+    @Deprecated(Messages.verifyPlugin, level = DeprecationLevel.ERROR)
+    abstract val verifierPath: Property<String>
+
+    @Deprecated(Messages.verifyPlugin, level = DeprecationLevel.ERROR)
+    abstract val freeArgs: ListProperty<String>
+
+    @Deprecated(Messages.verifyPlugin, level = DeprecationLevel.ERROR)
+    abstract val distributionFile: Property<Any>
+
+    @Deprecated(Messages.verifyPlugin, level = DeprecationLevel.ERROR)
+    abstract val verificationReportsDir: Property<String>
+
+    @Deprecated(Messages.verifyPlugin, level = DeprecationLevel.ERROR)
+    abstract val verificationReportsFormats: ListProperty<Any>
+
+    @Deprecated(Messages.verifyPlugin, level = DeprecationLevel.ERROR)
+    abstract val downloadDir: Property<String>
+
+    @Deprecated(Messages.verifyPlugin, level = DeprecationLevel.ERROR)
+    abstract val downloadPath: Property<Any>
+
+    @Deprecated(Messages.verifyPlugin, level = DeprecationLevel.ERROR)
+    abstract val jbrVersion: Property<String>
+
+    @Deprecated(Messages.verifyPlugin, level = DeprecationLevel.ERROR)
+    abstract val jbrVariant: Property<String>
+
+    @Deprecated(Messages.verifyPlugin, level = DeprecationLevel.ERROR)
+    abstract val jbrArch: Property<String>
+
+    @Deprecated(Messages.verifyPlugin, level = DeprecationLevel.ERROR)
+    abstract val jreRepository: Property<String>
+
+    @Deprecated(Messages.verifyPlugin, level = DeprecationLevel.ERROR)
+    abstract val runtimeDir: Property<String>
+
+    @Deprecated(Messages.verifyPlugin, level = DeprecationLevel.ERROR)
+    abstract val resolvedRuntimeDir: Property<Any>
+
+    @Deprecated(Messages.verifyPlugin, level = DeprecationLevel.ERROR)
+    abstract val externalPrefixes: ListProperty<String>
+
+    @Deprecated(Messages.verifyPlugin, level = DeprecationLevel.ERROR)
+    abstract val teamCityOutputFormat: Property<Boolean>
+
+    @Deprecated(Messages.verifyPlugin, level = DeprecationLevel.ERROR)
+    abstract val subsystemsToCheck: Property<String>
+
+    @Deprecated(Messages.verifyPlugin, level = DeprecationLevel.ERROR)
+    abstract val ignoredProblems: Property<Any>
+
+    @Deprecated(Messages.verifyPlugin, level = DeprecationLevel.ERROR)
+    abstract val ideDir: Property<Any>
+
+    @Deprecated(Messages.verifyPlugin, level = DeprecationLevel.ERROR)
+    abstract val offline: Property<Boolean>
 }
