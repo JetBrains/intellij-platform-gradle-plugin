72b6f849c09d949c75d57592687b9232122fdc40
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2024-02-20 23:32:52 +0100

    KDocs

--- src/main/kotlin/org/jetbrains/intellij/platform/gradle/BuildFeature.kt
+++ src/main/kotlin/org/jetbrains/intellij/platform/gradle/BuildFeature.kt
@@ -22,7 +22,8 @@ import org.jetbrains.intellij.platform.gradle.IntelliJPluginConstants.PLUGIN_ID 
 enum class BuildFeature(private val defaultValue: Boolean) {
     /**
      * Instruct IDE that sources are needed to be downloaded when working with IntelliJ Platform Gradle Plugin.
-     * Value is passed directly to the Idea Gradle Plugin to the `idea.module.downloadSources` property.
+     * Value is passed directly to the [Idea Gradle Plugin](https://docs.gradle.org/current/userguide/idea_plugin.html)
+     * to the `idea.module.downloadSources` property.
      *
      * @see <a href="https://docs.gradle.org/current/dsl/org.gradle.plugins.ide.idea.model.IdeaModule.html#org.gradle.plugins.ide.idea.model.IdeaModule:downloadSources">IdeaModule.downloadSources</a>
      */
