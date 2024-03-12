314732a4432e868620d1f4f5130c0b46c284e65f
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2024-03-12 17:52:06 +0100

    Tests: ModuleDescriptorsValueSourceTest cleanup

--- src/test/kotlin/org/jetbrains/intellij/platform/gradle/providers/ModuleDescriptorsValueSourceTest.kt
+++ src/test/kotlin/org/jetbrains/intellij/platform/gradle/providers/ModuleDescriptorsValueSourceTest.kt
@@ -3,6 +3,7 @@
 package org.jetbrains.intellij.platform.gradle.providers
 
 import org.jetbrains.intellij.platform.gradle.IntelliJPluginTestBase
+import java.nio.file.Path
 import kotlin.io.path.readLines
 import kotlin.test.Test
 import kotlin.test.assertContains
@@ -12,6 +13,21 @@ class ModuleDescriptorsValueSourceTest : IntelliJPluginTestBase() {
 
     @Test
     fun `convert module-descriptors file into a set of coordinates`() {
+        prepareAndBuild() {
+            val content = readLines()
+
+            assertContains(content, "com.jetbrains.intellij.platform:boot")
+            assertContains(content, "com.jetbrains.intellij.platform:util")
+            assertContains(content, "junit:junit")
+            assertContains(content, "org.jetbrains:jetCheck")
+            assertContains(content, "org.hamcrest:hamcrest-core")
+        }
+    }
+
+    private fun prepareAndBuild(block: Path.() -> Unit = {}) {
+        val taskName = "generateModuleDescriptors"
+        val outputFileName = "output.txt"
+
         buildFile.kotlin(
             """
             tasks {
@@ -22,7 +38,7 @@ class ModuleDescriptorsValueSourceTest : IntelliJPluginTestBase() {
                     }
                 }
             
-                val generateModuleDescriptors by registering {
+                register("generateModuleDescriptors") {
                     doLast {
                         val content = moduleDescriptors.get().joinToString(System.lineSeparator())
                         outputFile.writeText(content)
@@ -32,15 +48,8 @@ class ModuleDescriptorsValueSourceTest : IntelliJPluginTestBase() {
             """.trimIndent()
         )
 
-        build("generateModuleDescriptors")
-
-        val outputFile = dir.resolve("output.txt")
-        val content = outputFile.readLines()
-
-        assertContains(content, "com.jetbrains.intellij.platform:boot")
-        assertContains(content, "com.jetbrains.intellij.platform:util")
-        assertContains(content, "junit:junit")
-        assertContains(content, "org.jetbrains:jetCheck")
-        assertContains(content, "org.hamcrest:hamcrest-core")
+        build(taskName) {
+            dir.resolve(outputFileName).apply(block)
+        }
     }
 }
