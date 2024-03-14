90be72de60a06c1709237aa11abead555e6e7975
Yann Cébron <yann.cebron@jetbrains.com>
2024-03-14 10:01:00 +0100

    PlatformKotlinVersions: cleanup

--- src/main/kotlin/org/jetbrains/intellij/platform/gradle/utils/PlatformKotlinVersions.kt
+++ src/main/kotlin/org/jetbrains/intellij/platform/gradle/utils/PlatformKotlinVersions.kt
@@ -2,23 +2,14 @@
 
 package org.jetbrains.intellij.platform.gradle.utils
 
-// Bundled Kotlin versions list synchronized with:
-// https://plugins.jetbrains.com/docs/intellij/kotlin.html#kotlin-standard-library
+/**
+ * Bundled Kotlin versions for supported target platforms, synchronized with [IntelliJ Platform SDK Docs](https://jb.gg/intellij-platform-kotlin-stdlib)
+ **/
 val PlatformKotlinVersions = mapOf(
     Version(241) to Version(1, 9, 22),
     Version(233) to Version(1, 9, 10),
     Version(232) to Version(1, 8, 20),
     Version(231) to Version(1, 8, 0),
-    Version(223) to Version(1, 7, 0),
-    Version(222) to Version(1, 6, 21),
-    Version(221) to Version(1, 6, 20),
-    Version(213) to Version(1, 5, 10),
-    Version(212) to Version(1, 5, 10),
-    Version(211) to Version(1, 4, 32),
-    Version(203) to Version(1, 4, 0),
-    Version(202) to Version(1, 3, 70),
-    Version(201) to Version(1, 3, 70),
-    Version(193) to Version(1, 3, 31),
-    Version(192) to Version(1, 3, 3),
-    Version(191) to Version(1, 3, 11),
+    Version(223) to Version(1, 7, 0)
+
 )
