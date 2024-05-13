4803172f42d86e8f48c9a7f5fc4d7d10cfb59d66
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2024-05-13 21:11:09 +0200

    Update `PlatformJavaVersions` and `PlatformKotlinVersions`

--- src/main/kotlin/org/jetbrains/intellij/platform/gradle/utils/PlatformKotlinVersions.kt
+++ src/main/kotlin/org/jetbrains/intellij/platform/gradle/utils/PlatformKotlinVersions.kt
@@ -6,6 +6,7 @@ package org.jetbrains.intellij.platform.gradle.utils
  * Bundled Kotlin versions for supported target platforms, synchronized with [IntelliJ Platform SDK Docs](https://jb.gg/intellij-platform-kotlin-stdlib)
  **/
 val PlatformKotlinVersions = mapOf(
+    Version(242) to Version(1, 9, 24),
     Version(241) to Version(1, 9, 22),
     Version(233) to Version(1, 9, 10),
     Version(232) to Version(1, 8, 20),
