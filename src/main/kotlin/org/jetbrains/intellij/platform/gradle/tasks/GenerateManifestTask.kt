1b70bf97af75641c8021aa7aab6fa677954994c1
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2024-05-28 11:18:08 +0200

    GenerateManifestTask: resolve properties values from `InitializeIntelliJPlatformPluginTask` and `VerifyPluginProjectConfigurationTask` tasks, small enhancements

--- src/main/kotlin/org/jetbrains/intellij/platform/gradle/tasks/GenerateManifestTask.kt
+++ src/main/kotlin/org/jetbrains/intellij/platform/gradle/tasks/GenerateManifestTask.kt
@@ -6,23 +6,16 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.Project
 import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.provider.Property
-import org.gradle.api.tasks.CacheableTask
-import org.gradle.api.tasks.Input
-import org.gradle.api.tasks.OutputFile
-import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.*
 import org.gradle.internal.jvm.Jvm
 import org.gradle.internal.os.OperatingSystem
 import org.gradle.kotlin.dsl.assign
-import org.gradle.kotlin.dsl.of
+import org.gradle.kotlin.dsl.named
 import org.gradle.kotlin.dsl.the
-import org.gradle.kotlin.dsl.withGroovyBuilder
-import org.jetbrains.intellij.platform.gradle.Constants.GradleProperties
 import org.jetbrains.intellij.platform.gradle.Constants.Plugin
-import org.jetbrains.intellij.platform.gradle.Constants.Plugins
 import org.jetbrains.intellij.platform.gradle.Constants.Tasks
 import org.jetbrains.intellij.platform.gradle.extensions.IntelliJPlatformExtension
 import org.jetbrains.intellij.platform.gradle.models.ProductInfo
-import org.jetbrains.intellij.platform.gradle.providers.CurrentPluginVersionValueSource
 import org.jetbrains.intellij.platform.gradle.toIntelliJPlatformType
 import org.jetbrains.intellij.platform.gradle.utils.asPath
 import kotlin.io.path.writeText
@@ -30,24 +23,51 @@ import kotlin.io.path.writeText
 @CacheableTask
 abstract class GenerateManifestTask : DefaultTask() {
 
+    /**
+     * The IntelliJ Platform Gradle Plugin version.
+     */
     @get:Input
-    abstract val gradlePluginVersion: Property<String>
+    abstract val pluginVersion: Property<String>
 
+    /**
+     * The version of currently used Gradle.
+     */
     @get:Input
     abstract val gradleVersion: Property<String>
 
-    @get:Input
-    abstract val kotlinStdlibBundled: Property<Boolean>
+    /**
+     * Indicates that the Kotlin Gradle Plugin is loaded and available.
+     */
+    @get:Internal
+    abstract val kotlinPluginAvailable: Property<Boolean>
 
+    /**
+     * `kotlin.stdlib.default.dependency` property value defined in the `gradle.properties` file.
+     */
     @get:Input
-    abstract val kotlinVersion: Property<String>
+    abstract val kotlinStdlibDefaultDependency: Property<Boolean>
 
+    /**
+     * The version of Kotlin used in the project.
+     */
+    @get:Input
+    abstract val kotlinVersion: Property<String?>
+
+    /**
+     * The [ProductInfo] instance of the current IntelliJ Platform.
+     */
     @get:Input
     abstract val productInfo: Property<ProductInfo>
 
+    /**
+     * Plugin version.
+     */
     @get:Input
     abstract val version: Property<String>
 
+    /**
+     * Location of the generated `MANIFEST.MF` file.
+     */
     @get:OutputFile
     abstract val generatedManifest: RegularFileProperty
 
@@ -60,12 +80,13 @@ abstract class GenerateManifestTask : DefaultTask() {
             Build-JVM: ${Jvm.current()}
             Build-OS: ${OperatingSystem.current()}
             Build-Plugin: ${Plugin.NAME}
-            Build-Plugin-Version: ${gradlePluginVersion.get()}
+            Build-Plugin-Version: ${pluginVersion.get()}
             Platform-Type: ${productInfo.map { it.productCode.toIntelliJPlatformType() }.get()}
             Platform-Version: ${productInfo.map { it.version }.get()}
             Platform-Build: ${productInfo.map { it.buildNumber }.get()}
-            Kotlin-Stdlib-Bundled: ${kotlinStdlibBundled.get()}
-            Kotlin-Version: ${kotlinVersion.get()}
+            Kotlin-Available: ${kotlinPluginAvailable.get()}
+            Kotlin-Stdlib-Bundled: ${kotlinPluginAvailable.get() && kotlinStdlibDefaultDependency.get()}
+            Kotlin-Version: ${kotlinVersion.orNull}
             """.trimIndent()
         )
     }
@@ -74,28 +95,17 @@ abstract class GenerateManifestTask : DefaultTask() {
         override fun register(project: Project) =
             project.registerTask<GenerateManifestTask>(Tasks.GENERATE_MANIFEST) {
                 val extension = project.the<IntelliJPlatformExtension>()
+                val initializeIntelliJPlatformPluginTaskProvider =
+                    project.tasks.named<InitializeIntelliJPlatformPluginTask>(Tasks.INITIALIZE_INTELLIJ_PLATFORM_PLUGIN)
+                val verifyPluginProjectConfigurationTaskProvider =
+                    project.tasks.named<VerifyPluginProjectConfigurationTask>(Tasks.VERIFY_PLUGIN_PROJECT_CONFIGURATION)
 
                 productInfo.convention(project.provider { extension.productInfo })
-                kotlinStdlibBundled.convention(
-                    project.providers
-                        .gradleProperty(GradleProperties.KOTLIN_STDLIB_DEFAULT_DEPENDENCY)
-                        .orElse("true")
-                        .map { it.toBoolean() }
-                )
-                kotlinVersion.convention(project.provider {
-                    when {
-                        project.pluginManager.hasPlugin(Plugins.External.KOTLIN) ->
-                            project.extensions
-                                .getByName("kotlin")
-                                .withGroovyBuilder { getProperty("coreLibrariesVersion") as String }
-
-                        else -> ""
-                    }
-                })
-
-                gradlePluginVersion.convention(project.providers.of(CurrentPluginVersionValueSource::class) {})
+                kotlinStdlibDefaultDependency.convention(verifyPluginProjectConfigurationTaskProvider.flatMap { it.kotlinStdlibDefaultDependency })
+                kotlinVersion.convention(verifyPluginProjectConfigurationTaskProvider.flatMap { it.kotlinVersion })
+                pluginVersion.convention(initializeIntelliJPlatformPluginTaskProvider.flatMap { it.pluginVersion })
                 gradleVersion.convention(project.provider { project.gradle.gradleVersion })
-                version.convention(project.provider { project.version.toString() })
+                version.convention(extension.pluginConfiguration.version)
 
                 generatedManifest = temporaryDir.resolve("MANIFEST.MF")
             }
