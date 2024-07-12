dbb4dfa8bdda144a74526e8fa9ae8f6c3ca3eb15
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2024-07-12 02:37:33 +0200

    Gradle 8.9

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -45,7 +45,7 @@ project {
     }
 
     val operatingSystems = listOf("Linux", "Windows", "macOS")
-    val gradleVersions = listOf("8.2", "8.8")
+    val gradleVersions = listOf("8.2", "8.9")
 
     val buildChain = sequential {
         operatingSystems.forEach { os ->
