24f1f24283993973b2aabb9b0b6a03d0dd0dcca1
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2024-07-19 12:11:15 +0200

    Remove `org.gradle.toolchains.foojay-resolver-convention`

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,6 @@ pluginManagement {
 
 plugins {
     id("com.gradle.enterprise") version("3.16.2")
-    id("org.gradle.toolchains.foojay-resolver-convention") version("0.8.0")
 }
 
 rootProject.name = "IntelliJPlatformGradlePlugin"
