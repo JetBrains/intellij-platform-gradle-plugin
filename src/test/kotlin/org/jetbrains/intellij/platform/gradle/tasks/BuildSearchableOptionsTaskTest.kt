976c587f1eec568a10120aaebc8839820d4c81f6
AlexanderBartash <AlexanderBartash@gmail.com>
2024-09-24 21:45:57 +0300

    Fix Gradle configuration cache warnings in JarSearchableOptionsTask & PrepareJarSearchableOptionsTask caused by lazy value calculation, which uses a task reference. It has been fixed by moving out the value of Task.enabled into a property and explicitly defining it as a task input. Also added explicit dependencies between buildSearchableOptions -> prepareJarSearchableOptions -> jarSearchableOptions.

--- src/test/kotlin/org/jetbrains/intellij/platform/gradle/tasks/BuildSearchableOptionsTaskTest.kt
+++ src/test/kotlin/org/jetbrains/intellij/platform/gradle/tasks/BuildSearchableOptionsTaskTest.kt
@@ -51,4 +51,59 @@ class BuildSearchableOptionsTaskTest : SearchableOptionsTestBase() {
             assertTaskOutcome(Tasks.JAR_SEARCHABLE_OPTIONS, TaskOutcome.SKIPPED)
         }
     }
+
+    @Test
+    fun `build searchable options produces XML if enabled via property and explicitly configured`() {
+        pluginXml write getPluginXmlWithSearchableConfigurable()
+
+        getTestSearchableConfigurableJava() write getSearchableConfigurableCode()
+
+        gradleProperties write //language=properties
+                """
+                org.jetbrains.intellij.platform.buildSearchableOptions = true
+                """.trimIndent()
+
+
+        buildFile write //language=kotlin
+                """
+                intellijPlatform {
+                    buildSearchableOptions = true
+                }
+                """.trimIndent()
+
+        build(Tasks.BUILD_SEARCHABLE_OPTIONS) {
+            assertContains("Searchable options index builder completed", output)
+        }
+
+        val xml = buildDirectory.resolve("tmp/${Tasks.BUILD_SEARCHABLE_OPTIONS}/projectName-1.0.0.jar/search/projectName-1.0.0.jar.searchableOptions.xml")
+        assertExists(xml)
+
+        xml.readText().let {
+            assertContains("<configurable id=\"test.searchable.configurable\" configurable_name=\"Test Searchable Configurable\">", it)
+            assertContains("hit=\"Label for Test Searchable Configurable\"", it)
+        }
+    }
+
+    @Test
+    fun `skip build searchable options if disabled via property and explicitly configured`() {
+
+        gradleProperties write //language=properties
+                """
+                org.jetbrains.intellij.platform.buildSearchableOptions = false
+                """.trimIndent()
+
+        buildFile write //language=kotlin
+                """
+                intellijPlatform {
+                    buildSearchableOptions = providers.gradleProperty("org.jetbrains.intellij.platform.buildSearchableOptions").map {
+                        it.toBoolean()
+                    }
+                }
+                """.trimIndent()
+
+        build(Tasks.BUILD_PLUGIN) {
+            assertTaskOutcome(Tasks.BUILD_SEARCHABLE_OPTIONS, TaskOutcome.SKIPPED)
+            assertTaskOutcome(Tasks.JAR_SEARCHABLE_OPTIONS, TaskOutcome.SKIPPED)
+        }
+    }
 }
