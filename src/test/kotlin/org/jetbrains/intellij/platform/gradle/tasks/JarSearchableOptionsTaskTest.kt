b2e8e9f76db62cca79e04bff4c9c3e5789660b62
AlexanderBartash <AlexanderBartash@gmail.com>
2024-09-24 21:45:57 +0300

    Fix Gradle configuration cache warnings in JarSearchableOptionsTask & PrepareJarSearchableOptionsTask caused by lazy value calculation, which uses a task reference. It has been fixed by moving out the value of Task.enabled into a property and explicitly defining it as a task input. Also added explicit dependencies between buildSearchableOptions -> prepareJarSearchableOptions -> jarSearchableOptions.

--- src/test/kotlin/org/jetbrains/intellij/platform/gradle/tasks/JarSearchableOptionsTaskTest.kt
+++ src/test/kotlin/org/jetbrains/intellij/platform/gradle/tasks/JarSearchableOptionsTaskTest.kt
@@ -29,4 +29,60 @@ class JarSearchableOptionsTaskTest : SearchableOptionsTestBase() {
             assertEquals(setOf("projectName-1.0.0-searchableOptions.jar", "projectName-1.0.0-base.jar", "projectName-1.0.0.jar"), collectPaths(it))
         }
     }
+
+    @Test
+    fun `jar searchable options produces archive if enabled via property and explicitly configured`() {
+        pluginXml write getPluginXmlWithSearchableConfigurable()
+
+        gradleProperties write //language=properties
+                """
+                org.jetbrains.intellij.platform.buildSearchableOptions = true
+                """.trimIndent()
+
+        buildFile write //language=kotlin
+                """
+                intellijPlatform {
+                    buildSearchableOptions = providers.gradleProperty("org.jetbrains.intellij.platform.buildSearchableOptions").map {
+                        it.toBoolean()
+                    }
+                }
+                """.trimIndent()
+
+        getTestSearchableConfigurableJava() write getSearchableConfigurableCode()
+
+        build(Tasks.JAR_SEARCHABLE_OPTIONS)
+
+        buildDirectory.resolve("libs").let {
+            assertExists(it)
+            assertEquals(setOf("projectName-1.0.0-searchableOptions.jar", "projectName-1.0.0-base.jar", "projectName-1.0.0.jar"), collectPaths(it))
+        }
+    }
+
+    @Test
+    fun `jar searchable options disabled via property and explicitly configured`() {
+        pluginXml write getPluginXmlWithSearchableConfigurable()
+
+        gradleProperties write //language=properties
+                """
+                org.jetbrains.intellij.platform.buildSearchableOptions = false
+                """.trimIndent()
+
+        buildFile write //language=kotlin
+                """
+                intellijPlatform {
+                    buildSearchableOptions = providers.gradleProperty("org.jetbrains.intellij.platform.buildSearchableOptions").map {
+                        it.toBoolean()
+                    }
+                }
+                """.trimIndent()
+
+        getTestSearchableConfigurableJava() write getSearchableConfigurableCode()
+
+        build(Tasks.JAR_SEARCHABLE_OPTIONS)
+
+        buildDirectory.resolve("libs").let {
+            assertExists(it)
+            assertEquals(setOf("projectName-1.0.0-base.jar", "projectName-1.0.0.jar"), collectPaths(it))
+        }
+    }
 }
