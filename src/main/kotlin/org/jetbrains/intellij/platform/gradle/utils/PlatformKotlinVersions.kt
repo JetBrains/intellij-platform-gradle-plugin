d6cf2f0218e27198bbbfcd2a87ca140607787595
Jakub Chrzanowski <jakub@chrzanowski.info>
2025-02-06 12:52:34 +0100

    PlatformKotlinVersions: update with 243 and 251

--- src/main/kotlin/org/jetbrains/intellij/platform/gradle/utils/PlatformKotlinVersions.kt
+++ src/main/kotlin/org/jetbrains/intellij/platform/gradle/utils/PlatformKotlinVersions.kt
@@ -6,6 +6,8 @@ package org.jetbrains.intellij.platform.gradle.utils
  * Bundled Kotlin versions for supported target platforms, synchronized with [IntelliJ Platform SDK Docs](https://jb.gg/intellij-platform-kotlin-stdlib)
  **/
 val PlatformKotlinVersions = mapOf(
+    Version(251) to Version(2, 1, 10),
+    Version(243) to Version(2, 0, 21),
     Version(242) to Version(1, 9, 24),
     Version(241) to Version(1, 9, 22),
     Version(233) to Version(1, 9, 10),
