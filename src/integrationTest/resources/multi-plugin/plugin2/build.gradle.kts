db586f37ec3b1d3df55fc1544363d635ed1abc3c
Finn Petersen <fp7@posteo.net>
2025-05-14 17:23:50 +0200

    Re-add properties for platform and version

--- src/integrationTest/resources/multi-plugin/plugin2/build.gradle.kts
+++ src/integrationTest/resources/multi-plugin/plugin2/build.gradle.kts
@@ -1,5 +1,8 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
+val intellijPlatformTypeProperty = providers.gradleProperty("intellijPlatform.type")
+val intellijPlatformVersionProperty = providers.gradleProperty("intellijPlatform.version")
+
 plugins {
     id("org.jetbrains.intellij.platform")
 }
@@ -14,7 +17,7 @@ repositories {
 
 dependencies {
     intellijPlatform {
-        intellijIdeaCommunity("2025.1")
+        create(intellijPlatformTypeProperty, intellijPlatformVersionProperty)
         bundledPlugin("com.intellij.java")
     }
 }
