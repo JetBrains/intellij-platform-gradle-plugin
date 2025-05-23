52a726c6d9c95666db525e513f052d6f0a5325f4
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2025-05-23 15:28:18 +0200

    Update `PrintProductsReleasesTaskTest` to configure release channel filter

--- src/test/kotlin/org/jetbrains/intellij/platform/gradle/tasks/PrintProductsReleasesTaskTest.kt
+++ src/test/kotlin/org/jetbrains/intellij/platform/gradle/tasks/PrintProductsReleasesTaskTest.kt
@@ -5,17 +5,27 @@ package org.jetbrains.intellij.platform.gradle.tasks
 import org.jetbrains.intellij.platform.gradle.Constants.Tasks
 import org.jetbrains.intellij.platform.gradle.IntelliJPluginTestBase
 import org.jetbrains.intellij.platform.gradle.assertContains
+import org.jetbrains.intellij.platform.gradle.buildFile
+import org.jetbrains.intellij.platform.gradle.write
 import kotlin.test.Test
 
 class PrintProductsReleasesTaskTest : IntelliJPluginTestBase() {
 
     @Test
     fun `print product releases`() {
+        buildFile write //language=kotlin
+                """
+                tasks {
+                    ${Tasks.PRINT_PRODUCTS_RELEASES} {
+                        channels = listOf(org.jetbrains.intellij.platform.gradle.models.ProductRelease.Channel.RELEASE)
+                    }
+                }
+                """.trimIndent()
+
         build(Tasks.PRINT_PRODUCTS_RELEASES) {
             assertContains(
                 """
                 > Task :${Tasks.PRINT_PRODUCTS_RELEASES}
-                IC-252.13776.59
                 IC-2025.1.1
                 IC-2024.3.5
                 IC-2024.2.6
