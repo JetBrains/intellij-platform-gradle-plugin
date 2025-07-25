b0928f55c6fba89be444415219ffbd99b7b403c9
Alajemba <alajemba.dev@gmail.com>
2025-07-25 23:20:54 +0000

    feat(problems): add Problems API integration and error reporting enhancements

---
+++ src/main/kotlin/org/jetbrains/intellij/platform/gradle/Problems.kt
@@ -0,0 +1,62 @@
+package org.jetbrains.intellij.platform.gradle
+
+import org.gradle.api.Action
+import org.gradle.api.problems.ProblemGroup
+import org.gradle.api.problems.ProblemId
+import org.gradle.api.problems.ProblemReporter
+import org.gradle.api.problems.ProblemSpec
+import org.gradle.api.problems.Severity
+import org.jetbrains.intellij.platform.gradle.tasks.VerifyPluginTask
+import java.lang.RuntimeException
+
+internal object ProblemGroups {
+    val IntelliJPlatformPlugin = ProblemGroup.create(
+        "intellij-platform-plugin-group",
+        "IntelliJ Platform Plugin Problems"
+    )
+}
+
+internal object ProblemIds {
+
+    object VerifyPluginTaskIds {
+        val InvalidPlugin = intelliJPlatformPluginId(
+            "invalid-plugin",
+            "Invalid Plugin"
+        )
+        val InvalidPluginVerifier = intelliJPlatformPluginId(
+            "invalid-plugin-verifier",
+            "Invalid Plugin Verifer"
+        )
+
+        val InvalidIDEs = intelliJPlatformPluginId(
+            "no-ides-found",
+            "No IDEs Found"
+        )
+
+        val FailingTaskOnFailureLevel = { level: VerifyPluginTask.FailureLevel ->
+            intelliJPlatformPluginId(
+                "failing-task-on-${level}",
+                level.sectionHeading
+            )
+        }
+    }
+
+    private fun intelliJPlatformPluginId(name: String, displayName: String): ProblemId {
+        return ProblemId.create(name, displayName, ProblemGroups.IntelliJPlatformPlugin)
+    }
+
+}
+
+internal fun ProblemReporter.reportError(
+    exception: Exception,
+    problemId: ProblemId,
+    spec: Action<ProblemSpec>
+): RuntimeException {
+    return throwing(exception, problemId){
+
+        spec.execute(this)
+
+        withException(exception)
+        severity(Severity.ERROR)
+    }
+}
