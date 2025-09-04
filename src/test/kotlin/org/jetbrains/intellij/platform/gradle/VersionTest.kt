c920a846cae9ae095243baa8774e4ede58caa4ae
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2025-09-04 16:42:05 +0200

    Add tests for `parseIdeNotation` validation and parsing logic

--- src/test/kotlin/org/jetbrains/intellij/platform/gradle/VersionTest.kt
+++ src/test/kotlin/org/jetbrains/intellij/platform/gradle/VersionTest.kt
@@ -2,9 +2,11 @@
 
 package org.jetbrains.intellij.platform.gradle
 
+import org.jetbrains.intellij.platform.gradle.extensions.parseIdeNotation
 import org.jetbrains.intellij.platform.gradle.utils.Version
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 import kotlin.test.assertTrue
 
 class VersionTest {
@@ -35,5 +37,14 @@ class VersionTest {
         assertEquals(Version.parse("2021.1.0-snapshot"), Version.parse("2021.1.0-SNAPSHOT"))
         assertTrue(Version.parse("2021.1.2-SNAPSHOT.2") > Version.parse("2021.1.2-SNAPSHOT.1"))
         assertTrue(Version.parse("2021.1.2") > Version.parse("2021.1.2-SNAPSHOT.1"))
+    }
+
+    @Test
+    fun `parse IDE notation`() {
+        assertEquals("IC-2025.2".parseIdeNotation(), IntelliJPlatformType.IntellijIdeaCommunity to "2025.2")
+        assertFailsWith<IllegalArgumentException> { "2025.2".parseIdeNotation() }
+        assertFailsWith<IllegalArgumentException> { "2025.3".parseIdeNotation() }
+        assertEquals("PS-2025.3".parseIdeNotation(), IntelliJPlatformType.PhpStorm to "2025.3")
+        assertEquals("IU-2025.3".parseIdeNotation(), IntelliJPlatformType.IntellijIdea to "2025.3")
     }
 }
