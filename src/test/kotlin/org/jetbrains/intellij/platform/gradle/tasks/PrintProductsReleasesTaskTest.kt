cc13eeb7989ca076f466e5c79ac18ac632745d1a
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2025-09-22 09:07:03 +0200

    Update test assertions in `PrintProductsReleasesTaskTest` to reflect updated release versions

--- src/test/kotlin/org/jetbrains/intellij/platform/gradle/tasks/PrintProductsReleasesTaskTest.kt
+++ src/test/kotlin/org/jetbrains/intellij/platform/gradle/tasks/PrintProductsReleasesTaskTest.kt
@@ -26,9 +26,9 @@ class PrintProductsReleasesTaskTest : IntelliJPluginTestBase() {
             assertContains(
                 """
                 > Task :${Tasks.PRINT_PRODUCTS_RELEASES}
-                IC-2025.2.1
+                IC-2025.2.2
                 IC-2025.1.5
-                IC-2024.3.6
+                IC-2024.3.7
                 IC-2024.2.6
                 IC-2024.1.7
                 IC-2023.3.8
