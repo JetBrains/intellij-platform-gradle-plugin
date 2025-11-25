8843e5c1c87a048bdff6a565ab3298d52fab870e
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2025-11-25 19:38:03 +0100

    Remove redundant Maven repository from pluginManagement in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,6 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
 pluginManagement {
-    repositories {
-        maven("https://cache-redirector.jetbrains.com/plugins.gradle.org")
-    }
-
     includeBuild("build-logic")
 }
 
